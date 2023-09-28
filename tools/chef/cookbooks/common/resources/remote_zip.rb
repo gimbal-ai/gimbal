gimbal-ai/gimbal

commit 80e4a0c561967eed06afdbc7d800d7b6b5438960

Add terraform to dev docker image (#137)
Summary: We want to start using terraform in one of the GH actions, to
check that the terraform has already been applied to the global state.
This adds tf to the docker image.

Relevant Issues: N/A

Type of change: /kind infra

Test Plan: Run the docker image, check that terraform is there

Signed-off-by: Michelle Nguyen <michelle@gimletlabs.ai>
GitOrigin-RevId: 230c446f47c63a703a7c179ff9d0a1124a8b634a

tools/chef/cookbooks/common/resources/remote_zip.rb
@@ -1,0 +1,46 @@
+# Copyright 2023- Gimlet Labs, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+unified_mode true
+provides :remote_zip
+
+default_action :create
+
+action :create do
+  zip_path =  "/tmp/#{new_resource.name}.zip"
+
+  remote_file zip_path do
+    source node[new_resource.name]["download_path"]
+    mode "0644"
+    checksum node[new_resource.name]["sha256"]
+  end
+
+  execute "install tool" do
+    command "unzip -d /opt/gml_dev/tools/#{new_resource.name} -o #{zip_path}"
+  end
+
+  link "/opt/gml_dev/bin/#{new_resource.name}" do
+    to "/opt/gml_dev/tools/#{new_resource.name}/#{new_resource.name}"
+    link_type :symbolic
+    owner node["owner"]
+    group node["group"]
+    action :create
+  end
+
+  file "#{zip_path}" do
+    action :delete
+  end
+end

docker.properties
@@ -1,3 +1,3 @@
-DOCKER_IMAGE_TAG=202309261935
-DEV_IMAGE_DIGEST=b46fff2bfded9ef0826727b1bf86096ebcc24f7bab8c139c7aa04b7a79f8f3cb
-DEV_IMAGE_WITH_EXTRAS_DIGEST=992d12adae9cb0a7dcee2a405e106b5879f7051431ff42f8d69ba3b5936a124e
+DOCKER_IMAGE_TAG=202309272010
+DEV_IMAGE_DIGEST=954db2c86b15c3ac5cda7d84a628d10218a65b5c9d99e96e3987a56c0c14c24e
+DEV_IMAGE_WITH_EXTRAS_DIGEST=79167f5f6b680d1fdf2e3b6489d4feed1690d50eb179177adb4cbd872e9493ec

tools/chef/cookbooks/gml_dev_extras/attributes/linux.rb
@@ -83,6 +83,11 @@ default["sops"]["download_path"] =
 default["sops"]["sha256"] =
   "6eacdd01b68fd140eb71bbca233bea897cccb75dbf9e00a02e648b2f9a8a6939"
 
+default["terraform"]["download_path"] =
+  "https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.zip"
++default["terraform"]["sha256"] =
+  "c0ed7bc32ee52ae255af9982c8c88a7a4c610485cf1d55feeb037eab75fa082c"
+
 default["trivy"]["download_path"] =
   "https://github.com/aquasecurity/trivy/releases/download/v0.39.0/trivy_0.39.0_Linux-64bit.tar.gz"
 default["trivy"]["sha256"] =

tools/chef/cookbooks/gml_dev_extras/recipes/default.rb
@@ -93,27 +93,9 @@ execute "configure docker-credential-gcr" do
   action :run
 end
 
-remote_file "/tmp/packer.zip" do
-  source node["packer"]["download_path"]
-  mode "0644"
-  checksum node["packer"]["sha256"]
-end
+common_remote_zip 'packer'
 
-execute "install packer" do
-  command "unzip -d /opt/gml_dev/tools/packer -o /tmp/packer.zip"
-end
-
-link "/opt/gml_dev/bin/packer" do
-  to "/opt/gml_dev/tools/packer/packer"
-  link_type :symbolic
-  owner node["owner"]
-  group node["group"]
-  action :create
-end
-
-file "/tmp/packer.zip" do
-  action :delete
-end
+common_remote_zip 'terraform'
 
 directory "/usr/local/lib/docker/cli-plugins" do
   action :create

commit a4bc64178c62d46f8a6a1a5d5c7158e9112fcd14

Fix license (#154)
Summary: TSIA, this was broken from #137

Relevant Issues: N/A

Type of change: /kind bug

Test Plan: linter passes without any modifications

GitOrigin-RevId: f27a401df4b0e136bcb793b7a2852d633b704fba

tools/chef/cookbooks/common/resources/remote_zip.rb
@@ -1,4 +1,4 @@
-# Copyright 2023- Gimlet Labs, Inc.
+# Copyright 2018- The Pixie Authors.
 # Modifications Copyright 2023- Gimlet Labs, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,6 +20,8 @@ provides :remote_zip
 
 default_action :create
 
+property :name, String, name_property: true
+
 action :create do
   zip_path =  "/tmp/#{new_resource.name}.zip"
 
@@ -29,7 +31,7 @@ action :create do
     checksum node[new_resource.name]["sha256"]
   end
 
-  execute "install tool" do
+  execute "unpack #{new_resource.name}" do
     command "unzip -d /opt/gml_dev/tools/#{new_resource.name} -o #{zip_path}"
   end
 
@@ -41,7 +43,7 @@ action :create do
     action :create
   end
 
-  file "#{zip_path}" do
+  file zip_path do
     action :delete
   end
 end