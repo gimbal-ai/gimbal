514db3a88cff3dadbc77cd3964255c08550c42f2
Zain Asgar <zasgar@gimletlabs.ai>
2023-09-27 22:08:28 -0700

    Update skaffold (#155)
    Summary: TSIA
    
    Relevant Issues: N/A
    
    Type of change: /kind build
    
    Test Plan: N/A
    
    Signed-off-by: Zain Asgar <zasgar@gimletlabs.ai>
    GitOrigin-RevId: 7400243a879343b75fd63d3d9f3c57d4d3560b21

--- tools/chef/cookbooks/gml_dev_extras/attributes/linux.rb
+++ tools/chef/cookbooks/gml_dev_extras/attributes/linux.rb
@@ -74,9 +74,9 @@ default["packer"]["sha256"] =
   "8a94b84542d21b8785847f4cccc8a6da4c7be5e16d4b1a2d0a5f7ec5532faec0"
 
 default["skaffold"]["download_path"] =
-  "https://storage.googleapis.com/skaffold/releases/v2.0.4/skaffold-linux-amd64"
+  "https://storage.googleapis.com/skaffold/releases/v2.7.1/skaffold-linux-amd64"
 default["skaffold"]["sha256"] =
-  "f1c54245b97073f39b209dd4b769802701c9901ceaa95774e7507d95bbd1d4c0"
+  "91b0353ea5dfd19b839e41942b8f7b71dbe54b1dfba56b9709bde0dacac513e6"
 
 default["sops"]["download_path"] =
   "https://github.com/mozilla/sops/releases/download/3.3.1/sops-3.3.1.linux"
