fa7f6314174765a574858c754426e551bcf13edb
James Bartlett <james@gimletlabs.ai>
2023-10-17 16:10:39 -0700

    [gem] Add ExecutionContext to TensorRT plugin (#591)
    Summary: Adds the `ExecutionContext` for the `TensorRT` plugin. This
    context wraps a built `TensorRT` engine. Additionally, it has a
    `CUDATensorPool`. Calculators can use this context to allocate
    `CUDATensor`s or execute a `TensorRT` engine.
    
    Relevant Issues: Towards GML-142
    
    Type of change: /kind feature
    
    Test Plan: We need to figure out how to test `TensorRT` code. For now,
    I'm going to punt on that issue.
    
    Signed-off-by: James Bartlett <james@gimletlabs.ai>
    GitOrigin-RevId: e5e7fe6256ceab069be1cd58602f9ec9ace72696

---
+++ src/gem/plugins/tensorrt/exec/context.cc
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2023- Gimlet Labs, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Gimlet Labs, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Gimlet Labs, Inc. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from
+ * Gimlet Labs, Inc.
+ *
+ * SPDX-License-Identifier: Proprietary
+ */
+
+#include <NvInfer.h>
+#include <NvOnnxParser.h>
+#include "src/common/base/base.h"
+
+#include "src/gem/plugins/tensorrt/exec/context.h"
+
+namespace gml {
+namespace gem {
+namespace tensorrt {
+
+cudaStream_t ExecutionContext::CUDAStream() {
+  // TODO(james): look into utilizing CUDA streams. For now everything runs fully
+  // synchronously on the default stream.
+  return nullptr;
+}
+
+}  // namespace tensorrt
+}  // namespace gem
+}  // namespace gml
