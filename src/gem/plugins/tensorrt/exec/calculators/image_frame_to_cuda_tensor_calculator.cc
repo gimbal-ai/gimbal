2dbff5cd75827d17c61ecccbab7a7d4ab3c9a6f1
James Bartlett <james@gimletlabs.ai>
2023-10-18 23:25:25 -0700

    [gem] Add ImageFrameToCUDATensor calculator (#601)
    Summary: Adds a converter calculator from mediapipe's ImageFrame format
    to the tensorrt plugin's CUDATensor format. We likely won't use this
    when we are pulling data from the camera, but it's useful for testing
    without a camera (eg. from mp4). The implementation is currently very
    ad-hoc and requires a specific input type, and outputs specifically to
    NCHW 3-channel float format.
    
    Relevant Issues: Towards GML-142.
    
    Type of change: /kind feature
    
    Test Plan: Tested on a jetson. No CI tests yet for targets that require
    TensorRT.
    
    Signed-off-by: James Bartlett <james@gimletlabs.ai>
    GitOrigin-RevId: e1cd2812d393c41a6179bdc84de90e9dea53fa2b

---
+++ src/gem/plugins/tensorrt/exec/calculators/image_frame_to_cuda_tensor_calculator.cc
@@ -0,0 +1,106 @@
+/*
+ * Copyright © 2023- Gimlet Labs, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Gimlet Labs, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Gimlet Labs, Inc. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from
+ * Gimlet Labs, Inc.
+ *
+ * SPDX-License-Identifier: Proprietary
+ */
+
+#include <cuda_runtime_api.h>
+
+#include <mediapipe/framework/calculator_registry.h>
+#include <mediapipe/framework/formats/image_frame.h>
+#include <mediapipe/framework/formats/image_frame_opencv.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include "src/common/base/base.h"
+#include "src/gem/plugins/tensorrt/exec/calculators/image_frame_to_cuda_tensor_calculator.h"
+#include "src/gem/plugins/tensorrt/exec/context.h"
+#include "src/gem/plugins/tensorrt/exec/cuda_tensor_pool.h"
+
+namespace gml {
+namespace gem {
+namespace tensorrt {
+namespace calculators {
+
+static constexpr char kImageFrameTag[] = "IMAGE_FRAME";
+static constexpr char kTensorTag[] = "OUTPUT_TENSOR";
+
+absl::Status ImageFrameToCUDATensorCalculator::GetContract(mediapipe::CalculatorContract* cc) {
+  GML_ABSL_RETURN_IF_ERROR(ExecutionContextBaseCalculator::UpdateContract(cc));
+
+  cc->Inputs().Tag(kImageFrameTag).Set<mediapipe::ImageFrame>();
+  cc->Outputs().Tag(kTensorTag).Set<CUDATensorPtr>();
+  return absl::OkStatus();
+}
+
+Status ImageFrameToCUDATensorCalculator::OpenImpl(mediapipe::CalculatorContext*,
+                                                  ExecutionContext*) {
+  return Status::OK();
+}
+
+Status ImageFrameToCUDATensorCalculator::ProcessImpl(mediapipe::CalculatorContext* cc,
+                                                     ExecutionContext* exec_ctx) {
+  const auto& image_frame = cc->Inputs().Tag(kImageFrameTag).Get<mediapipe::ImageFrame>();
+
+  if (image_frame.NumberOfChannels() != 3) {
+    return Status(types::CODE_INVALID_ARGUMENT, "Only 3 channel images are currently supported");
+  }
+  auto mat_view = mediapipe::formats::MatView(&image_frame);
+  if (!mat_view.isContinuous()) {
+    return Status(types::CODE_INVALID_ARGUMENT,
+                  "Only contiguous opencv matrices are supported for now");
+  }
+
+  // TODO(james): currently only supporting 3 channel RGB images with values in 0-255.
+  if (mat_view.type() != CV_8UC3) {
+    return Status(types::CODE_INVALID_ARGUMENT,
+                  "Only 3 channel unsigned char images are currently supported.");
+  }
+
+  // TODO(james): we should do these conversions separately.
+  cv::Mat float_img;
+  mat_view.convertTo(float_img, CV_32F, 1.0 / 255);
+
+  size_t chan_size = float_img.rows * float_img.cols * float_img.elemSize1();
+  size_t bytes = chan_size * float_img.channels();
+  GML_ASSIGN_OR_RETURN(auto tensor, exec_ctx->TensorPool()->GetTensor(bytes));
+
+  // TODO(james): This is inefficient. We should convert directly to CUDA NHWC and then use the GPU
+  // to convert to NCHW.
+  cv::Mat channels[3];
+  cv::split(float_img, channels);
+
+  for (const auto& [i, chan] : Enumerate(channels)) {
+    if (cudaMemcpy(static_cast<uint8_t*>(tensor->data()) + i * chan_size, chan.ptr<float>(0),
+                   chan_size, cudaMemcpyHostToDevice) != cudaSuccess) {
+      return Status(types::CODE_INTERNAL, "Failed to memcpy from host to cuda device");
+    }
+  }
+
+  auto packet = mediapipe::MakePacket<CUDATensorPtr>(std::move(tensor));
+  packet = packet.At(cc->InputTimestamp());
+  cc->Outputs().Tag(kTensorTag).AddPacket(std::move(packet));
+  return Status::OK();
+}
+Status ImageFrameToCUDATensorCalculator::CloseImpl(mediapipe::CalculatorContext*,
+                                                   ExecutionContext*) {
+  return Status::OK();
+}
+
+REGISTER_CALCULATOR(ImageFrameToCUDATensorCalculator);
+
+}  // namespace calculators
+}  // namespace tensorrt
+}  // namespace gem
+}  // namespace gml
---
+++ src/gem/plugins/tensorrt/exec/calculators/image_frame_to_cuda_tensor_calculator.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2023- Gimlet Labs, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Gimlet Labs, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Gimlet Labs, Inc. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from
+ * Gimlet Labs, Inc.
+ *
+ * SPDX-License-Identifier: Proprietary
+ */
+
+#include "src/gem/plugins/tensorrt/exec/calculators/base.h"
+#include "src/gem/plugins/tensorrt/exec/context.h"
+
+namespace gml {
+namespace gem {
+namespace tensorrt {
+namespace calculators {
+
+/**
+ * ImageFrameToCUDATensorCalculator Graph API:
+ *
+ * Input Side Packets:
+ *  EXEC_CTX
+ *    Must be a pointer to tensorrt::ExecutionContext
+ * Inputs:
+ *  IMAGE_FRAME
+ *    image in mediapipes::ImageFrame format.
+ * Outputs:
+ *  OUTPUT_TENSOR
+ *    CUDATensorPtr
+ **/
+class ImageFrameToCUDATensorCalculator : public ExecutionContextBaseCalculator {
+ public:
+  static absl::Status GetContract(mediapipe::CalculatorContract* cc);
+  Status OpenImpl(mediapipe::CalculatorContext* cc, tensorrt::ExecutionContext* exec_ctx) override;
+  Status ProcessImpl(mediapipe::CalculatorContext* cc,
+                     tensorrt::ExecutionContext* exec_ctx) override;
+  Status CloseImpl(mediapipe::CalculatorContext* cc, tensorrt::ExecutionContext* exec_ctx) override;
+};
+
+}  // namespace calculators
+}  // namespace tensorrt
+}  // namespace gem
+}  // namespace gml
