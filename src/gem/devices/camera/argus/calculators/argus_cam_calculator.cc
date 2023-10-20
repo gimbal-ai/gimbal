62d1cf7f554ef11a00f84e1edc0af37e0ba60846
Omid Azizi <oazizi@gimletlabs.ai>
2023-10-20 13:37:41 -0700

    [gem] Mediapipe source calculator for Argus camera (#616)
    Summary: A basic MediaPipe source calculator for the Argus camera
    capture.
    
    Relevant Issues: Towards GML-165
    
    Type of change: /kind feature
    
    Test Plan: Still manual testing for these changes, since they require a
    Jeston device.
    
    Signed-off-by: Omid Azizi <oazizi@gimletlabs.ai>
    GitOrigin-RevId: 1beceef4422fba75335bd120cb9636759d66cdc1

---
+++ src/gem/devices/camera/argus/calculators/argus_cam_calculator.cc
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2023- Gimlet Labs, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Gimlet Labs, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Gimlet Labs, Inc. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from
+ * Gimlet Labs, Inc.
+ *
+ * SPDX-License-Identifier: Proprietary
+ */
+
+#include "src/gem/devices/camera/argus/calculators/argus_cam_calculator.h"
+
+#include "absl/status/status.h"
+
+#include <mediapipe/framework/calculator_framework.h>
+#include "src/gem/devices/camera/argus/argus_cam.h"
+
+namespace gml {
+namespace gem {
+
+absl::Status ArgusCamSourceCalculator::GetContract(mediapipe::CalculatorContract* cc) {
+  cc->Outputs().Index(0).Set<NvBufSurfaceWrapper>();
+  return absl::OkStatus();
+}
+
+absl::Status ArgusCamSourceCalculator::Open(mediapipe::CalculatorContext* /* cc */) {
+  GML_ABSL_RETURN_IF_ERROR(argus_cam_.Init());
+  return absl::OkStatus();
+}
+
+absl::Status ArgusCamSourceCalculator::Process(mediapipe::CalculatorContext* cc) {
+  absl::Status s;
+
+  GML_ABSL_ASSIGN_OR_RETURN(std::unique_ptr<NvBufSurfaceWrapper> buf, argus_cam_.ConsumeFrame());
+  cc->Outputs().Index(0).Add(buf.release(), cc->InputTimestamp());
+
+  return absl::OkStatus();
+}
+
+absl::Status ArgusCamSourceCalculator::Close(mediapipe::CalculatorContext* /* cc */) {
+  argus_cam_.Stop();
+  return absl::OkStatus();
+}
+
+REGISTER_CALCULATOR(ArgusCamSourceCalculator);
+
+}  // namespace gem
+}  // namespace gml
