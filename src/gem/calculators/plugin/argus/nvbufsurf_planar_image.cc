ea478d1a0e100d6aaa0520869e0748679f606516
Omid Azizi <oazizi@gimletlabs.ai>
2023-10-27 15:02:15 -0700

    [gem] Implementation of PlanarImage for NvBufSurfaceWrapper (#828)
    Summary: This implementation of PlanarImage for NvBufSurfaceWrapper will
    enable us to hook up the ArgusCamCalucaltor to other calculators.
    
    Relevant Issues: Towards GML-222
    
    Type of change: /kind feature
    
    Test Plan: Test is not functional because it requires Argus (which
    doesn't work on x86), and the device specific test is missing a shared
    library to become functional. The test will be submitted as follow-up
    work.
    
    Changelog Message:
    
    GitOrigin-RevId: 36fb7f4ce097d2ec333704187d42e7de82fd74dd

---
+++ src/gem/calculators/plugin/argus/nvbufsurf_planar_image.cc
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2023- Gimlet Labs, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Gimlet Labs, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Gimlet Labs, Inc. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from
+ * Gimlet Labs, Inc.
+ *
+ * SPDX-License-Identifier: Proprietary
+ */
+
+#include "src/common/base/base.h"
+#include "src/gem/devices/camera/argus/nvbufsurfwrapper.h"
+#include "src/gem/exec/core/planar_image.h"
+
+// This file provides an implementation of PlanarImage for NvBufSurfaceWrapper.
+
+namespace gml {
+namespace gem {
+namespace exec {
+namespace core {
+
+using ::gml::gem::devices::argus::NvBufSurfaceWrapper;
+
+template <>
+Status PlanarImageFor<NvBufSurfaceWrapper>::BuildPlanes() {
+  constexpr int kNumPlanes = 3;
+
+  const NvBufSurfaceWrapper* image = image_.get();
+
+  planes_.resize(kNumPlanes);
+
+  for (int i = 0; i < kNumPlanes; ++i) {
+    planes_[i].data = reinterpret_cast<uint8_t*>(image->surface().mappedAddr.addr[i]);
+    planes_[i].row_stride = image->surface().planeParams.pitch[i];
+
+    if (planes_[i].data != nullptr) {
+      return error::Internal("Image plane pointer $0 is nullptr.", i);
+    }
+  }
+
+  return Status::OK();
+}
+
+template <>
+size_t PlanarImageFor<NvBufSurfaceWrapper>::Width() const {
+  return image_->surface().width;
+}
+
+template <>
+size_t PlanarImageFor<NvBufSurfaceWrapper>::Height() const {
+  return image_->surface().height;
+}
+
+}  // namespace core
+}  // namespace exec
+}  // namespace gem
+}  // namespace gml
