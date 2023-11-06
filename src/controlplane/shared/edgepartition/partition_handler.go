0a3f009f09cc8039c2daeae13839eac5b81a8fc0
Michelle Nguyen <michelle@gimletlabs.ai>
2023-11-05 22:20:48 -0800

    Move partitionHandler's message handling into a separate goroutine (#1063)
    Summary: The previous PR refactored the fleetwatcher + modelserver code
    to use a unified partition handler. While updating the devicestream
    server to use the partitionhandler, I realized the partitionhandler
    should make sure not to block the NATS listening goroutine with any
    message handling--otherwise NATS will begin dropping messages. This
    moves the message handling into a separate goroutine.
    
    Relevant Issues: N/A
    
    Type of change: /kind cleanup
    
    Test Plan: unit tests still pass
    
    Signed-off-by: Michelle Nguyen <michelle@gimletlabs.ai>
    GitOrigin-RevId: facbfe1f4581408fb0595d15246575da7e01d397

--- src/controlplane/shared/edgepartition/partition_handler.go
+++ src/controlplane/shared/edgepartition/partition_handler.go
@@ -38,6 +38,7 @@ type PartitionHandler struct {
 	nc              *nats.Conn
 	e2cpTopic       corepb.EdgeCPTopic
 	messageHandlers map[string]MessageHandler
+	msgCh           chan *corepb.EdgeCPMessage
 
 	// Signal used to quit.
 	done chan struct{}
@@ -46,7 +47,7 @@ type PartitionHandler struct {
 
 // NewPartitionHandler creates a new partition handler for the given topic.
 func NewPartitionHandler(nc *nats.Conn, e2cpTopic corepb.EdgeCPTopic, messageHandlers map[string]MessageHandler) *PartitionHandler {
-	return &PartitionHandler{nc: nc, e2cpTopic: e2cpTopic, messageHandlers: messageHandlers}
+	return &PartitionHandler{nc: nc, e2cpTopic: e2cpTopic, messageHandlers: messageHandlers, msgCh: make(chan *corepb.EdgeCPMessage, channelSize)}
 }
 
 // Start starts the listening and handling for messages from any edge devices.
@@ -88,13 +89,25 @@ func (p *PartitionHandler) startPartitionHandler(partition string, subCh chan *n
 				}
 				return
 			case msg := <-subCh:
-				// Handle the message.
 				e2CpMsg := &corepb.EdgeCPMessage{}
 				err = e2CpMsg.Unmarshal(msg.Data)
 				if err != nil {
 					topicLog.WithError(err).Error("Failed to unmarshal CPEdgeMessage... Skipping.")
 					continue
 				}
+				// Put message on message handling queue. We don't want to block the NATS reading goroutine, or else
+				// NATS will begin dropping messages.
+				p.msgCh <- e2CpMsg
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-p.done:
+				return
+			case e2CpMsg := <-p.msgCh:
 				msgType, err := types.AnyMessageName(e2CpMsg.Msg)
 				if err != nil {
 					topicLog.WithError(err).WithField("deviceID", e2CpMsg.Metadata.DeviceID).Error("Failed to get type of any message")
