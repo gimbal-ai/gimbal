6fc5df434c4665c54a4fa4eefa7b1f7f99623034
Zain Asgar <zasgar@gimletlabs.ai>
2023-11-14 18:41:32 -0800

    [GEM-Controller] Add Heartbeat message handler (#1220)
    Summary: Adds ta heartbeat sender and handler. Implementation sends a
    heartbeat and then waits for an ACK. We still need to add handling for
    missing hearbeats
    and broken connections.
    
    Relevant Issues: towards GML-229
    
    Type of change: /kind feature
    
    Test Plan: None yet, we need to figure out how to properly test the
    controller code.
    
    Signed-off-by: Zain Asgar <zasgar@gimletlabs.ai>
    GitOrigin-RevId: 2fe5775654460261342eb31bfefd18bbded69e7a

---
+++ src/gem/controller/heartbeat.cc
@@ -0,0 +1,101 @@
+
+/*
+ * Copyright © 2023- Gimlet Labs, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Gimlet Labs, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Gimlet Labs, Inc. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from
+ * Gimlet Labs, Inc.
+ *
+ * SPDX-License-Identifier: Proprietary
+ */
+
+#include <unistd.h>
+
+#include <google/protobuf/any.pb.h>
+#include <grpcpp/grpcpp.h>
+#include <chrono>
+
+#include "src/api/corepb/v1/cp_edge.pb.h"
+#include "src/common/base/base.h"
+#include "src/common/event/event.h"
+#include "src/common/uuid/uuid.h"
+#include "src/controlplane/egw/egwpb/v1/egwpb.grpc.pb.h"
+#include "src/controlplane/fleetmgr/fmpb/v1/fmpb.grpc.pb.h"
+#include "src/controlplane/fleetmgr/fmpb/v1/fmpb.pb.h"
+#include "src/gem/controller/grpc_bridge.h"
+#include "src/gem/controller/heartbeat.h"
+#include "src/gem/controller/message_handler.h"
+
+namespace gml::gem::controller {
+
+using gml::internal::api::core::v1::CP_EDGE_TOPIC_STATUS;
+using gml::internal::api::core::v1::CPEdgeTopic;
+using gml::internal::api::core::v1::EDGE_CP_TOPIC_STATUS;
+using gml::internal::api::core::v1::EdgeCPMessage;
+using gml::internal::api::core::v1::EdgeHeartbeat;
+using gml::internal::api::core::v1::EdgeHeartbeatAck;
+
+using gml::internal::controlplane::egw::v1::BridgeRequest;
+using gml::internal::controlplane::egw::v1::BridgeResponse;
+
+controller::HeartbeatHandler::HeartbeatHandler(event::Dispatcher* d, GEMInfo* agent_info,
+                                               GRPCBridge* bridge)
+    : MessageHandler(d, agent_info, bridge),
+      time_source_(d->GetTimeSource()),
+      heartbeat_send_timer_(dispatcher()->CreateTimer([this] { SendHeartbeat(); })) {}
+
+Status HeartbeatHandler::HandleMessage(const BridgeResponse& msg) {
+  EdgeHeartbeatAck ack;
+  if (!msg.msg().UnpackTo(&ack)) {
+    LOG(ERROR) << "Failed to unpack heartbeat ack message. Received message of type: "
+               << msg.msg().type_url() << " . Ignoring...";
+    return Status::OK();
+  }
+
+  auto time_delta = time_source_.MonotonicTime() - heartbeat_info_.last_heartbeat_send_time_;
+  heartbeat_latency_moving_average_ =
+      kHbLatencyDecay * heartbeat_latency_moving_average_ + (1 - kHbLatencyDecay) * time_delta;
+  LOG_EVERY_N(INFO, 5) << absl::StrFormat(
+      "Heartbeat ACK latency moving average: %d ms",
+      std::chrono::duration_cast<std::chrono::milliseconds>(heartbeat_latency_moving_average_)
+          .count());
+  heartbeat_info_.last_ackd_seq_num = ack.seq_id();
+  return Status::OK();
+}
+
+void HeartbeatHandler::EnableHeartbeats() {
+  heartbeat_send_timer_->EnableTimer(std::chrono::milliseconds(0));
+}
+
+Status HeartbeatHandler::SendHeartbeatImpl() {
+  // HB Code Start.
+  if (heartbeat_info_.last_ackd_seq_num < heartbeat_info_.last_sent_seq_num) {
+    // Send over the previous request again.
+    return bridge()->SendMessageToBridge(EDGE_CP_TOPIC_STATUS, *heartbeat_info_.active_hb_);
+  }
+
+  heartbeat_info_.active_hb_ = std::make_unique<EdgeHeartbeat>();
+  heartbeat_info_.last_sent_seq_num++;
+  heartbeat_info_.active_hb_->set_seq_id(heartbeat_info_.last_sent_seq_num);
+  heartbeat_info_.last_heartbeat_send_time_ = time_source_.MonotonicTime();
+
+  return bridge()->SendMessageToBridge(EDGE_CP_TOPIC_STATUS, *heartbeat_info_.active_hb_);
+}
+
+void HeartbeatHandler::SendHeartbeat() {
+  Status s = SendHeartbeatImpl();
+  if (!s.ok()) {
+    LOG(ERROR) << "Failed to send heartbeat, will retry on next tick. hb_info: "
+               << heartbeat_info_.active_hb_->DebugString() << " error_message: " << s.msg();
+  }
+  heartbeat_send_timer_->EnableTimer(kHeartbeatInterval);
+}
+
+}  // namespace gml::gem::controller
---
+++ src/gem/controller/heartbeat.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright © 2023- Gimlet Labs, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Gimlet Labs, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Gimlet Labs, Inc. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from
+ * Gimlet Labs, Inc.
+ *
+ * SPDX-License-Identifier: Proprietary
+ */
+
+#include <grpcpp/grpcpp.h>
+#include <string>
+#include <string_view>
+
+#include <sole.hpp>
+
+#include "src/api/corepb/v1/cp_edge.pb.h"
+#include "src/controlplane/egw/egwpb/v1/egwpb.grpc.pb.h"
+#include "src/gem/controller/gem_info.h"
+#include "src/gem/controller/message_handler.h"
+
+namespace gml::gem::controller {
+
+class HeartbeatHandler : public MessageHandler {
+ public:
+  HeartbeatHandler() = delete;
+  HeartbeatHandler(gml::event::Dispatcher*, GEMInfo*, GRPCBridge*);
+
+  ~HeartbeatHandler() override = default;
+
+  Status Init() override { return Status::OK(); }
+
+  Status HandleMessage(const gml::internal::controlplane::egw::v1::BridgeResponse& msg) override;
+
+  Status Finish() override { return Status::OK(); }
+
+  void EnableHeartbeats();
+
+ private:
+  struct HeartbeatInfo {
+    HeartbeatInfo() : active_hb_(std::make_unique<gml::internal::api::core::v1::EdgeHeartbeat>()) {}
+
+    int64_t last_sent_seq_num = -1;
+    int64_t last_ackd_seq_num = -1;
+    std::chrono::steady_clock::time_point last_heartbeat_send_time_;
+    std::chrono::duration<double> heartbeat_latency_moving_average_{0};
+    std::unique_ptr<gml::internal::api::core::v1::EdgeHeartbeat> active_hb_;
+  };
+
+  HeartbeatInfo heartbeat_info_;
+  const gml::event::TimeSource& time_source_;
+  Status SendHeartbeatImpl();
+  void SendHeartbeat();
+
+  gml::event::TimerUPtr heartbeat_send_timer_;
+  std::chrono::duration<double> heartbeat_latency_moving_average_{0};
+
+  static constexpr double kHbLatencyDecay = 0.25;
+
+  static constexpr std::chrono::seconds kHeartbeatInterval{2};
+  static constexpr int kHeartbeatRetryCount = 5;
+
+  // TODO(zasgar): Implement heartbeat watch dog.
+  // gml::event::TimerUPtr heartbeat_watchdog_timer_;
+  // static constexpr std::chrono::milliseconds kHeartbeatWaitMillis{5000};
+};
+
+}  // namespace gml::gem::controller
