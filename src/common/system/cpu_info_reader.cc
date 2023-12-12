ac126603cf91d97dd741d3d38f32f86c3e5d8143
Zain Asgar <zasgar@gimletlabs.ai>
2023-12-12 14:02:27 -0800

    Add CPU info reader interface (#1813)
    Summary: This adds the interface for the CPU info reader. Currently the
    only thing it can do is read the cpu frequencies (after implementation).
    
    Relevant Issues: towards GML-311
    
    Type of change: /kind feature
    
    Test Plan: N/A
    
    Signed-off-by: Zain Asgar <zasgar@gimletlabs.ai>
    GitOrigin-RevId: 2739fa3682144d903b7448d38350a5dcb66882c4

---
+++ src/common/system/cpu_info_reader.cc
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2023- Gimlet Labs, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Gimlet Labs, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Gimlet Labs, Inc. and its suppliers and
+ * may be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law. Dissemination
+ * of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from
+ * Gimlet Labs, Inc.
+ *
+ * SPDX-License-Identifier: Proprietary
+ */
+
+#include "src/common/system/cpu_info_reader.h"
+#include "src/common/base/macros.h"
+
+namespace gml::system {
+
+Status CPUInfoReader::Init() { return Status::OK(); }
+
+Status CPUInfoReader::ReadCPUFrequencies(std::vector<CPUFrequencyInfo>* cpu_freqs) const {
+  GML_UNUSED(cpu_freqs);
+  return Status::OK();
+}
+}  // namespace gml::system
