gimbal-ai/gimbal

commit bd50a07de2c4bd26e15bcd8bdbfece55ba567a42

Autogenerate tag prefixes based on select sysroot for oci_push (#1866)
Summary: Added a macro to autogen tags based on sysroots.

Relevant Issues: N/A

Type of change: /kind cleanup

Test Plan: Ran `bazel run --config=jetson_sysroot //src/gem:push_gem_image`
and `bazel run //src/gem:push_gem_image` and checked the tags.

Signed-off-by: Vihang Mehta <vihang@gimletlabs.ai>
GitOrigin-RevId: 4e0c5e8e5ef3ac630eb36dd69c86bd0e0e6f63b7

.bazelrc
@@ -61,6 +61,7 @@ build:linux --action_env=BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN=1
 # Non sysroot config.
 build:no_sysroot --//bazel/cc_toolchains:libc_version=glibc_host
 build:no_sysroot --config=clang-libc++
+build:jetson_sysroot --//bazel/cc_toolchains/sysroots:sysroot_type=none
 
 # We build our own chroot for the sysroot tests, which doesn't work well under bazel's sandbox.
 build:sysroot-base --//bazel/test_runners:test_runner=sysroot_chroot

bazel/cc_toolchains/sysroots/BUILD.bazel
@@ -34,10 +34,18 @@ string_flag(
     name = "sysroot_type",
     build_setting_default = "debian12",
     values = [
+        "none",
         "debian12",
         "jetson",
         "intelgpu",
     ],
+)
+
+config_setting(
+    name = "sysroot_type_none",
+    flag_values = {
+        ":sysroot_type": "none",
+    },
 )
 
 config_setting(

bazel/gml_build_system.bzl
@@ -347,7 +347,7 @@ def _add_no_sysroot(kwargs):
 
 def _no_sysroot():
     return select({
-        "//bazel/cc_toolchains:libc_version_glibc_host": [],
+        "//bazel/cc_toolchains/sysroots:sysroot_type_none": [],
         "//conditions:default": ["@platforms//:incompatible"],
     })
 

bazel/images.bzl
@@ -13,7 +13,8 @@
 #
 # SPDX-License-Identifier: Proprietary
 
-load("@rules_oci//oci:defs.bzl", "oci_image", "oci_image_index", "oci_tarball")
+load("@bazel_skylib//rules:write_file.bzl", "write_file")
+load("@rules_oci//oci:defs.bzl", "oci_image", "oci_image_index", "oci_push", "oci_tarball")
 load("@rules_pkg//pkg:providers.bzl", "PackageFilesInfo")
 load("@rules_pkg//pkg:tar.bzl", "pkg_tar")
 load("//bazel:lib.bzl", "default_arg")
@@ -104,6 +105,23 @@ def _gml_binary_image(name, binary, multiarch = False, **kwargs):
     kwargs["tars"] = kwargs["tars"] + [name + "_binary_tar"]
     _gml_oci_image(name, multiarch = multiarch, testonly = testonly, **kwargs)
 
+def _gml_oci_push(name, **kwargs):
+    tags = kwargs.pop("remote_tags", [])
+
+    write_file(
+        name = name + "_write_tags",
+        out = name + "_tags.txt",
+        content = select({
+            "//bazel/cc_toolchains/sysroots:sysroot_type_debian12": ["debian12-" + tag for tag in tags],
+            "//bazel/cc_toolchains/sysroots:sysroot_type_intelgpu": ["intelgpu-" + tag for tag in tags],
+            "//bazel/cc_toolchains/sysroots:sysroot_type_jetson": ["jetson-" + tag for tag in tags],
+            "//bazel/cc_toolchains/sysroots:sysroot_type_none": ["nosysroot-" + tag for tag in tags],
+            "//conditions:default": tags,
+        }),
+    )
+
+    oci_push(name = name, remote_tags = name + "_write_tags", **kwargs)
+
 def _collect_runfiles_impl(ctx):
     dest_to_src = dict()
     for binary in ctx.attr.binaries:
@@ -152,3 +170,4 @@ _collect_runfiles = rule(
 
 gml_oci_image = _gml_oci_image
 gml_binary_image = _gml_binary_image
+gml_oci_push = _gml_oci_push

src/gem/BUILD.bazel
@@ -12,10 +12,8 @@
 # Gimlet Labs, Inc.
 #
 # SPDX-License-Identifier: Proprietary
-
-load("@rules_oci//oci:defs.bzl", "oci_push")
-load("//bazel:gml_build_system.bzl", "gml_cc_binary", "gml_cc_library", "intelgpu_sysroot", "jetson_sysroot")
-load("//bazel:images.bzl", "gml_binary_image")
+load("//bazel:gml_build_system.bzl", "gml_cc_binary", "gml_cc_library")
+load("//bazel:images.bzl", "gml_binary_image", "gml_oci_push")
 
 package(default_visibility = [
     "//src:__subpackages__",
@@ -53,27 +51,9 @@ gml_binary_image(
     include_runfiles = True,
 )
 
-oci_push(
+gml_oci_push(
     name = "push_gem_image",
     image = ":gem_image",
     remote_tags = ["dev-latest"],
     repository = "us-docker.pkg.dev/gimlet-dev-infra-0/gimlet-dev-infra-public-docker-artifacts/gem_image",
-)
-
-# TODO(james): remove once remote_tags changes have settled.
-oci_push(
-    name = "push_gem_intelgpu_image",
-    image = ":gem_image",
-    remote_tags = ["intelgpu-dev-latest"],
-    repository = "us-docker.pkg.dev/gimlet-dev-infra-0/gimlet-dev-infra-public-docker-artifacts/gem_image",
-    target_compatible_with = intelgpu_sysroot(),
-)
-
-# TODO(james): remove once remote_tags changes have settled.
-oci_push(
-    name = "push_gem_jetson_image",
-    image = ":gem_image",
-    remote_tags = ["jetson-dev-latest"],
-    repository = "us-docker.pkg.dev/gimlet-dev-infra-0/gimlet-dev-infra-public-docker-artifacts/gem_image",
-    target_compatible_with = jetson_sysroot(),
 )

src/gem/fakegem/BUILD.bazel
@@ -13,9 +13,8 @@
 #
 # SPDX-License-Identifier: Proprietary
 
-load("@rules_oci//oci:defs.bzl", "oci_push")
 load("//bazel:gml_build_system.bzl", "gml_cc_binary", "gml_cc_library")
-load("//bazel:images.bzl", "gml_binary_image")
+load("//bazel:images.bzl", "gml_binary_image", "gml_oci_push")
 
 package(default_visibility = [
     "//src:__subpackages__",
@@ -58,7 +57,7 @@ gml_binary_image(
     include_runfiles = True,
 )
 
-oci_push(
+gml_oci_push(
     name = "push_fakegem_image",
     image = ":fakegem_image",
     remote_tags = ["fake-latest"],

commit cb7f000139eebac63e4d34721939dbd184dca212

Fix typo (#1868)
Summary: Accidentally introduced this in #1866
... sigh ...

Relevant Issues: N/A

Type of change: /kind bug

Test Plan: The all_debs.test target works again.

Signed-off-by: Vihang Mehta <vihang@gimletlabs.ai>
GitOrigin-RevId: 63dd81ee81c0d0e928061c9c9e862e4d22171bab

.bazelrc
@@ -61,7 +61,6 @@ build:linux --action_env=BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN=1
 # Non sysroot config.
 build:no_sysroot --//bazel/cc_toolchains:libc_version=glibc_host
 build:no_sysroot --config=clang-libc++
-build:jetson_sysroot --//bazel/cc_toolchains/sysroots:sysroot_type=none
 
 # We build our own chroot for the sysroot tests, which doesn't work well under bazel's sandbox.
 build:sysroot-base --//bazel/test_runners:test_runner=sysroot_chroot

bazel/cc_toolchains/sysroots/BUILD.bazel
@@ -34,18 +34,10 @@ string_flag(
     name = "sysroot_type",
     build_setting_default = "debian12",
     values = [
-        "none",
         "debian12",
         "jetson",
         "intelgpu",
     ],
-)
-
-config_setting(
-    name = "sysroot_type_none",
-    flag_values = {
-        ":sysroot_type": "none",
-    },
 )
 
 config_setting(

bazel/gml_build_system.bzl
@@ -347,7 +347,7 @@ def _add_no_sysroot(kwargs):
 
 def _no_sysroot():
     return select({
-        "//bazel/cc_toolchains/sysroots:sysroot_type_none": [],
+        "//bazel/cc_toolchains:libc_version_glibc_host": [],
         "//conditions:default": ["@platforms//:incompatible"],
     })
 

bazel/images.bzl
@@ -115,7 +115,6 @@ def _gml_oci_push(name, **kwargs):
             "//bazel/cc_toolchains/sysroots:sysroot_type_debian12": ["debian12-" + tag for tag in tags],
             "//bazel/cc_toolchains/sysroots:sysroot_type_intelgpu": ["intelgpu-" + tag for tag in tags],
             "//bazel/cc_toolchains/sysroots:sysroot_type_jetson": ["jetson-" + tag for tag in tags],
-            "//bazel/cc_toolchains/sysroots:sysroot_type_none": ["nosysroot-" + tag for tag in tags],
             "//conditions:default": tags,
         }),
     )