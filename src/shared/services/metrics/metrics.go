d7dda0c5e5fd51f31a49b4c57336a3ac6681f329
Michelle Nguyen <michelle@gimletlabs.ai>
2024-03-01 14:16:44 -0800

    [crucible] Add prometheus metrics handler for Go (#2348)
    Summary: To track our crucible metrics, we will be using prometheus.
    This adds the prometheus handler so that it can be used by our Go
    services.
    
    Relevant Issues: towards GML-411
    
    Type of change: /kind feature
    
    Test Plan: Run a test service with prometheus metrics, check prometheus,
    and verify the metrics are recorded
    
    Signed-off-by: Michelle Nguyen <michelle@gimletlabs.ai>
    GitOrigin-RevId: ee0e26ccdba8da4e42bf3ff03d841ad3fa4d69ff

---
+++ src/shared/services/metrics/metrics.go
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018- The Pixie Authors.
+ * Modifications Copyright 2023- Gimlet Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package metrics
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// mux is an interface describing the methods InstallHandler requires.
+type mux interface {
+	Handle(pattern string, handler http.Handler)
+}
+
+// MustRegisterMetricsHandler registers the metrics endpoint.
+func MustRegisterMetricsHandler(mux mux) {
+	mux.Handle("/metrics", promhttp.HandlerFor(
+		prometheus.DefaultGatherer,
+		promhttp.HandlerOpts{
+			// Opt into OpenMetrics to support exemplars.
+			EnableOpenMetrics: true,
+		},
+	))
+	prometheus.MustRegister(collectors.NewBuildInfoCollector())
+}
+
+// MustRegisterMetricsHandlerNoDefaultMetrics registers the metrics endpoint but removes all of the default metrics that prometheus includes, so only custom metrics are collected and reported.
+func MustRegisterMetricsHandlerNoDefaultMetrics(mux mux) {
+	mux.Handle("/metrics", promhttp.HandlerFor(
+		prometheus.DefaultGatherer,
+		promhttp.HandlerOpts{
+			// Opt into OpenMetrics to support exemplars.
+			EnableOpenMetrics: true,
+		},
+	))
+
+	// Unregister the default metrics the prometheus golang client provides.
+	prometheus.Unregister(collectors.NewGoCollector())
+	prometheus.Unregister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+}
