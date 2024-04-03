f90e56161e50654d1e49b06fee2dbccdd2d62943
Vihang Mehta <vihang@gimletlabs.ai>
2024-04-03 11:31:21 -0700

    Propagate logical pipeline ID to the gem when applying exec graph (#2779)
    Summary: We would like to associate GEM metrics with the logical
    pipeline that is running. To do so, we must propagate the logical
    pipeline ID from the controlplane to the GEM.
    
    Relevant Issues: Towards GML-557
    
    Type of change: /kind infra
    
    Test Plan: Updated existing unit tests.
    
    Signed-off-by: Vihang Mehta <vihang@gimletlabs.ai>
    GitOrigin-RevId: fa44b8a8ee125803e2d085d7c66cdfec7674950d

--- src/api/corepb/v1/cp_edge_pb2.py
+++ src/api/corepb/v1/cp_edge_pb2.py
@@ -20,7 +20,7 @@ from opentelemetry.proto.metrics.v1 import metrics_pb2 as opentelemetry_dot_prot
 from src.api.corepb.v1 import model_exec_pb2 as src_dot_api_dot_corepb_dot_v1_dot_model__exec__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1fsrc/api/corepb/v1/cp_edge.proto\x12\x18gml.internal.api.core.v1\x1a\x14gogoproto/gogo.proto\x1a\x1dsrc/common/typespb/uuid.proto\x1a\x1fsrc/common/typespb/status.proto\x1a\x19google/protobuf/any.proto\x1a\x1fgoogle/protobuf/timestamp.proto\x1a,opentelemetry/proto/metrics/v1/metrics.proto\x1a\"src/api/corepb/v1/model_exec.proto\"1\n\rEdgeHeartbeat\x12 \n\x06seq_id\x18\x01 \x01(\x03\x42\t\xe2\xde\x1f\x05SeqIDR\x05seqId\"4\n\x10\x45\x64geHeartbeatAck\x12 \n\x06seq_id\x18\x01 \x01(\x03\x42\t\xe2\xde\x1f\x05SeqIDR\x05seqId\"\x0c\n\nCPRunModel\"\x0f\n\rCPRunModelAck\"\xb2\x01\n\x12\x45xecutionGraphSpec\x12=\n\x05graph\x18\x01 \x01(\x0b\x32\'.gml.internal.api.core.v1.ExecutionSpecR\x05graph\x12\x43\n\x05state\x18\x02 \x01(\x0e\x32-.gml.internal.api.core.v1.ExecutionGraphStateR\x05state\x12\x18\n\x07version\x18\x03 \x01(\x03R\x07version\"\x8d\x01\n\x14\x45xecutionGraphStatus\x12\x43\n\x05state\x18\x01 \x01(\x0e\x32-.gml.internal.api.core.v1.ExecutionGraphStateR\x05state\x12\x16\n\x06reason\x18\x02 \x01(\tR\x06reason\x12\x18\n\x07version\x18\x03 \x01(\x03R\x07version\"\xb2\x01\n\x13\x41pplyExecutionGraph\x12Y\n\x14physical_pipeline_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x16\xe2\xde\x1f\x12PhysicalPipelineIDR\x12physicalPipelineId\x12@\n\x04spec\x18\x02 \x01(\x0b\x32,.gml.internal.api.core.v1.ExecutionGraphSpecR\x04spec\"q\n\x14\x44\x65leteExecutionGraph\x12Y\n\x14physical_pipeline_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x16\xe2\xde\x1f\x12PhysicalPipelineIDR\x12physicalPipelineId\"\xbf\x01\n\x1a\x45xecutionGraphStatusUpdate\x12Y\n\x14physical_pipeline_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x16\xe2\xde\x1f\x12PhysicalPipelineIDR\x12physicalPipelineId\x12\x46\n\x06status\x18\x02 \x01(\x0b\x32..gml.internal.api.core.v1.ExecutionGraphStatusR\x06status\"\x12\n\x10VideoStreamStart\"\x11\n\x0fVideoStreamStop\"\x16\n\x14VideoStreamKeepAlive\"m\n\x0f\x45\x64geOTelMetrics\x12Z\n\x10resource_metrics\x18\x01 \x01(\x0b\x32/.opentelemetry.proto.metrics.v1.ResourceMetricsR\x0fresourceMetrics\"\x94\x01\n\x13\x46ileTransferRequest\x12\x34\n\x07\x66ile_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\n\xe2\xde\x1f\x06\x46ileIDR\x06\x66ileId\x12*\n\x11\x63hunk_start_bytes\x18\x02 \x01(\x03R\x0f\x63hunkStartBytes\x12\x1b\n\tnum_bytes\x18\x03 \x01(\x03R\x08numBytes\"\x8f\x02\n\x14\x46ileTransferResponse\x12)\n\x06status\x18\x01 \x01(\x0b\x32\x11.gml.types.StatusR\x06status\x12N\n\x05\x63hunk\x18\x02 \x01(\x0b\x32\x38.gml.internal.api.core.v1.FileTransferResponse.FileChunkR\x05\x63hunk\x12\x34\n\x07\x66ile_id\x18\x03 \x01(\x0b\x32\x0f.gml.types.UUIDB\n\xe2\xde\x1f\x06\x46ileIDR\x06\x66ileId\x1a\x46\n\tFileChunk\x12\x1f\n\x0bstart_bytes\x18\x01 \x01(\x03R\nstartBytes\x12\x18\n\x07payload\x18\x02 \x01(\x0cR\x07payload\"\xb0\x05\n\x12\x44\x65viceCapabilities\x12\x64\n\x0emodel_runtimes\x18\x01 \x03(\x0b\x32=.gml.internal.api.core.v1.DeviceCapabilities.ModelRuntimeInfoR\rmodelRuntimes\x12Q\n\x07\x63\x61meras\x18\x02 \x03(\x0b\x32\x37.gml.internal.api.core.v1.DeviceCapabilities.CameraInfoR\x07\x63\x61meras\x1a\xec\x01\n\x10ModelRuntimeInfo\x12\x62\n\x04type\x18\x01 \x01(\x0e\x32N.gml.internal.api.core.v1.DeviceCapabilities.ModelRuntimeInfo.ModelRuntimeTypeR\x04type\"t\n\x10ModelRuntimeType\x12\x1e\n\x1aMODEL_RUNTIME_TYPE_UNKNOWN\x10\x00\x12\x1f\n\x1bMODEL_RUNTIME_TYPE_TENSORRT\x10\x01\x12\x1f\n\x1bMODEL_RUNTIME_TYPE_OPENVINO\x10\x02\x1a\xf1\x01\n\nCameraInfo\x12\\\n\x06\x64river\x18\x01 \x01(\x0e\x32\x44.gml.internal.api.core.v1.DeviceCapabilities.CameraInfo.CameraDriverR\x06\x64river\x12)\n\tcamera_id\x18\x02 \x01(\tB\x0c\xe2\xde\x1f\x08\x43\x61meraIDR\x08\x63\x61meraId\"Z\n\x0c\x43\x61meraDriver\x12\x19\n\x15\x43\x41MERA_DRIVER_UNKNOWN\x10\x00\x12\x17\n\x13\x43\x41MERA_DRIVER_ARGUS\x10\x01\x12\x16\n\x12\x43\x41MERA_DRIVER_V4L2\x10\x02\"\xcc\x01\n\x0e\x45\x64geCPMetadata\x12;\n\x05topic\x18\x01 \x01(\x0e\x32%.gml.internal.api.core.v1.EdgeCPTopicR\x05topic\x12:\n\tdevice_id\x18\x02 \x01(\x0b\x32\x0f.gml.types.UUIDB\x0c\xe2\xde\x1f\x08\x44\x65viceIDR\x08\x64\x65viceId\x12\x41\n\x0erecv_timestamp\x18\x03 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\rrecvTimestamp\"~\n\rEdgeCPMessage\x12\x44\n\x08metadata\x18\x01 \x01(\x0b\x32(.gml.internal.api.core.v1.EdgeCPMetadataR\x08metadata\x12\'\n\x03msg\x18\xe8\x07 \x01(\x0b\x32\x14.google.protobuf.AnyR\x03msg\"\xcc\x01\n\x0e\x43PEdgeMetadata\x12;\n\x05topic\x18\x01 \x01(\x0e\x32%.gml.internal.api.core.v1.CPEdgeTopicR\x05topic\x12:\n\tdevice_id\x18\x02 \x01(\x0b\x32\x0f.gml.types.UUIDB\x0c\xe2\xde\x1f\x08\x44\x65viceIDR\x08\x64\x65viceId\x12\x41\n\x0erecv_timestamp\x18\x03 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\rrecvTimestamp\"~\n\rCPEdgeMessage\x12\x44\n\x08metadata\x18\x01 \x01(\x0b\x32(.gml.internal.api.core.v1.CPEdgeMetadataR\x08metadata\x12\'\n\x03msg\x18\xe8\x07 \x01(\x0b\x32\x14.google.protobuf.AnyR\x03msg*\xbe\x02\n\x13\x45xecutionGraphState\x12!\n\x1d\x45XECUTION_GRAPH_STATE_UNKNOWN\x10\x00\x12*\n&EXECUTION_GRAPH_STATE_UPDATE_REQUESTED\x10\n\x12%\n!EXECUTION_GRAPH_STATE_DOWNLOADING\x10\x14\x12#\n\x1f\x45XECUTION_GRAPH_STATE_COMPILING\x10\x1e\x12\x1f\n\x1b\x45XECUTION_GRAPH_STATE_READY\x10(\x12\"\n\x1e\x45XECUTION_GRAPH_STATE_DEPLOYED\x10\x32\x12%\n!EXECUTION_GRAPH_STATE_TERMINATING\x10<\x12 \n\x1c\x45XECUTION_GRAPH_STATE_FAILED\x10\x64*\xc7\x01\n\x0b\x45\x64geCPTopic\x12\x19\n\x15\x45\x44GE_CP_TOPIC_UNKNOWN\x10\x00\x12\x18\n\x14\x45\x44GE_CP_TOPIC_STATUS\x10\x01\x12\x17\n\x13\x45\x44GE_CP_TOPIC_VIDEO\x10\x02\x12\x16\n\x12\x45\x44GE_CP_TOPIC_EXEC\x10\x03\x12\x19\n\x15\x45\x44GE_CP_TOPIC_METRICS\x10\x04\x12\x1f\n\x1b\x45\x44GE_CP_TOPIC_FILE_TRANSFER\x10\x05\x12\x16\n\x12\x45\x44GE_CP_TOPIC_INFO\x10\x06*\xc7\x01\n\x0b\x43PEdgeTopic\x12\x19\n\x15\x43P_EDGE_TOPIC_UNKNOWN\x10\x00\x12\x18\n\x14\x43P_EDGE_TOPIC_STATUS\x10\x01\x12\x17\n\x13\x43P_EDGE_TOPIC_VIDEO\x10\x02\x12\x16\n\x12\x43P_EDGE_TOPIC_EXEC\x10\x03\x12\x19\n\x15\x43P_EDGE_TOPIC_METRICS\x10\x04\x12\x1f\n\x1b\x43P_EDGE_TOPIC_FILE_TRANSFER\x10\x05\x12\x16\n\x12\x43P_EDGE_TOPIC_INFO\x10\x06\x42/Z-gimletlabs.ai/gimlet/src/api/corepb/v1;corepbb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1fsrc/api/corepb/v1/cp_edge.proto\x12\x18gml.internal.api.core.v1\x1a\x14gogoproto/gogo.proto\x1a\x1dsrc/common/typespb/uuid.proto\x1a\x1fsrc/common/typespb/status.proto\x1a\x19google/protobuf/any.proto\x1a\x1fgoogle/protobuf/timestamp.proto\x1a,opentelemetry/proto/metrics/v1/metrics.proto\x1a\"src/api/corepb/v1/model_exec.proto\"1\n\rEdgeHeartbeat\x12 \n\x06seq_id\x18\x01 \x01(\x03\x42\t\xe2\xde\x1f\x05SeqIDR\x05seqId\"4\n\x10\x45\x64geHeartbeatAck\x12 \n\x06seq_id\x18\x01 \x01(\x03\x42\t\xe2\xde\x1f\x05SeqIDR\x05seqId\"\x0c\n\nCPRunModel\"\x0f\n\rCPRunModelAck\"\xb2\x01\n\x12\x45xecutionGraphSpec\x12=\n\x05graph\x18\x01 \x01(\x0b\x32\'.gml.internal.api.core.v1.ExecutionSpecR\x05graph\x12\x43\n\x05state\x18\x02 \x01(\x0e\x32-.gml.internal.api.core.v1.ExecutionGraphStateR\x05state\x12\x18\n\x07version\x18\x03 \x01(\x03R\x07version\"\x8d\x01\n\x14\x45xecutionGraphStatus\x12\x43\n\x05state\x18\x01 \x01(\x0e\x32-.gml.internal.api.core.v1.ExecutionGraphStateR\x05state\x12\x16\n\x06reason\x18\x02 \x01(\tR\x06reason\x12\x18\n\x07version\x18\x03 \x01(\x03R\x07version\"\x8a\x02\n\x13\x41pplyExecutionGraph\x12Y\n\x14physical_pipeline_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x16\xe2\xde\x1f\x12PhysicalPipelineIDR\x12physicalPipelineId\x12V\n\x13logical_pipeline_id\x18\x03 \x01(\x0b\x32\x0f.gml.types.UUIDB\x15\xe2\xde\x1f\x11LogicalPipelineIDR\x11logicalPipelineId\x12@\n\x04spec\x18\x02 \x01(\x0b\x32,.gml.internal.api.core.v1.ExecutionGraphSpecR\x04spec\"q\n\x14\x44\x65leteExecutionGraph\x12Y\n\x14physical_pipeline_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x16\xe2\xde\x1f\x12PhysicalPipelineIDR\x12physicalPipelineId\"\xbf\x01\n\x1a\x45xecutionGraphStatusUpdate\x12Y\n\x14physical_pipeline_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x16\xe2\xde\x1f\x12PhysicalPipelineIDR\x12physicalPipelineId\x12\x46\n\x06status\x18\x02 \x01(\x0b\x32..gml.internal.api.core.v1.ExecutionGraphStatusR\x06status\"\x12\n\x10VideoStreamStart\"\x11\n\x0fVideoStreamStop\"\x16\n\x14VideoStreamKeepAlive\"m\n\x0f\x45\x64geOTelMetrics\x12Z\n\x10resource_metrics\x18\x01 \x01(\x0b\x32/.opentelemetry.proto.metrics.v1.ResourceMetricsR\x0fresourceMetrics\"\x94\x01\n\x13\x46ileTransferRequest\x12\x34\n\x07\x66ile_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\n\xe2\xde\x1f\x06\x46ileIDR\x06\x66ileId\x12*\n\x11\x63hunk_start_bytes\x18\x02 \x01(\x03R\x0f\x63hunkStartBytes\x12\x1b\n\tnum_bytes\x18\x03 \x01(\x03R\x08numBytes\"\x8f\x02\n\x14\x46ileTransferResponse\x12)\n\x06status\x18\x01 \x01(\x0b\x32\x11.gml.types.StatusR\x06status\x12N\n\x05\x63hunk\x18\x02 \x01(\x0b\x32\x38.gml.internal.api.core.v1.FileTransferResponse.FileChunkR\x05\x63hunk\x12\x34\n\x07\x66ile_id\x18\x03 \x01(\x0b\x32\x0f.gml.types.UUIDB\n\xe2\xde\x1f\x06\x46ileIDR\x06\x66ileId\x1a\x46\n\tFileChunk\x12\x1f\n\x0bstart_bytes\x18\x01 \x01(\x03R\nstartBytes\x12\x18\n\x07payload\x18\x02 \x01(\x0cR\x07payload\"\xb0\x05\n\x12\x44\x65viceCapabilities\x12\x64\n\x0emodel_runtimes\x18\x01 \x03(\x0b\x32=.gml.internal.api.core.v1.DeviceCapabilities.ModelRuntimeInfoR\rmodelRuntimes\x12Q\n\x07\x63\x61meras\x18\x02 \x03(\x0b\x32\x37.gml.internal.api.core.v1.DeviceCapabilities.CameraInfoR\x07\x63\x61meras\x1a\xec\x01\n\x10ModelRuntimeInfo\x12\x62\n\x04type\x18\x01 \x01(\x0e\x32N.gml.internal.api.core.v1.DeviceCapabilities.ModelRuntimeInfo.ModelRuntimeTypeR\x04type\"t\n\x10ModelRuntimeType\x12\x1e\n\x1aMODEL_RUNTIME_TYPE_UNKNOWN\x10\x00\x12\x1f\n\x1bMODEL_RUNTIME_TYPE_TENSORRT\x10\x01\x12\x1f\n\x1bMODEL_RUNTIME_TYPE_OPENVINO\x10\x02\x1a\xf1\x01\n\nCameraInfo\x12\\\n\x06\x64river\x18\x01 \x01(\x0e\x32\x44.gml.internal.api.core.v1.DeviceCapabilities.CameraInfo.CameraDriverR\x06\x64river\x12)\n\tcamera_id\x18\x02 \x01(\tB\x0c\xe2\xde\x1f\x08\x43\x61meraIDR\x08\x63\x61meraId\"Z\n\x0c\x43\x61meraDriver\x12\x19\n\x15\x43\x41MERA_DRIVER_UNKNOWN\x10\x00\x12\x17\n\x13\x43\x41MERA_DRIVER_ARGUS\x10\x01\x12\x16\n\x12\x43\x41MERA_DRIVER_V4L2\x10\x02\"\xcc\x01\n\x0e\x45\x64geCPMetadata\x12;\n\x05topic\x18\x01 \x01(\x0e\x32%.gml.internal.api.core.v1.EdgeCPTopicR\x05topic\x12:\n\tdevice_id\x18\x02 \x01(\x0b\x32\x0f.gml.types.UUIDB\x0c\xe2\xde\x1f\x08\x44\x65viceIDR\x08\x64\x65viceId\x12\x41\n\x0erecv_timestamp\x18\x03 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\rrecvTimestamp\"~\n\rEdgeCPMessage\x12\x44\n\x08metadata\x18\x01 \x01(\x0b\x32(.gml.internal.api.core.v1.EdgeCPMetadataR\x08metadata\x12\'\n\x03msg\x18\xe8\x07 \x01(\x0b\x32\x14.google.protobuf.AnyR\x03msg\"\xcc\x01\n\x0e\x43PEdgeMetadata\x12;\n\x05topic\x18\x01 \x01(\x0e\x32%.gml.internal.api.core.v1.CPEdgeTopicR\x05topic\x12:\n\tdevice_id\x18\x02 \x01(\x0b\x32\x0f.gml.types.UUIDB\x0c\xe2\xde\x1f\x08\x44\x65viceIDR\x08\x64\x65viceId\x12\x41\n\x0erecv_timestamp\x18\x03 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\rrecvTimestamp\"~\n\rCPEdgeMessage\x12\x44\n\x08metadata\x18\x01 \x01(\x0b\x32(.gml.internal.api.core.v1.CPEdgeMetadataR\x08metadata\x12\'\n\x03msg\x18\xe8\x07 \x01(\x0b\x32\x14.google.protobuf.AnyR\x03msg*\xbe\x02\n\x13\x45xecutionGraphState\x12!\n\x1d\x45XECUTION_GRAPH_STATE_UNKNOWN\x10\x00\x12*\n&EXECUTION_GRAPH_STATE_UPDATE_REQUESTED\x10\n\x12%\n!EXECUTION_GRAPH_STATE_DOWNLOADING\x10\x14\x12#\n\x1f\x45XECUTION_GRAPH_STATE_COMPILING\x10\x1e\x12\x1f\n\x1b\x45XECUTION_GRAPH_STATE_READY\x10(\x12\"\n\x1e\x45XECUTION_GRAPH_STATE_DEPLOYED\x10\x32\x12%\n!EXECUTION_GRAPH_STATE_TERMINATING\x10<\x12 \n\x1c\x45XECUTION_GRAPH_STATE_FAILED\x10\x64*\xc7\x01\n\x0b\x45\x64geCPTopic\x12\x19\n\x15\x45\x44GE_CP_TOPIC_UNKNOWN\x10\x00\x12\x18\n\x14\x45\x44GE_CP_TOPIC_STATUS\x10\x01\x12\x17\n\x13\x45\x44GE_CP_TOPIC_VIDEO\x10\x02\x12\x16\n\x12\x45\x44GE_CP_TOPIC_EXEC\x10\x03\x12\x19\n\x15\x45\x44GE_CP_TOPIC_METRICS\x10\x04\x12\x1f\n\x1b\x45\x44GE_CP_TOPIC_FILE_TRANSFER\x10\x05\x12\x16\n\x12\x45\x44GE_CP_TOPIC_INFO\x10\x06*\xc7\x01\n\x0b\x43PEdgeTopic\x12\x19\n\x15\x43P_EDGE_TOPIC_UNKNOWN\x10\x00\x12\x18\n\x14\x43P_EDGE_TOPIC_STATUS\x10\x01\x12\x17\n\x13\x43P_EDGE_TOPIC_VIDEO\x10\x02\x12\x16\n\x12\x43P_EDGE_TOPIC_EXEC\x10\x03\x12\x19\n\x15\x43P_EDGE_TOPIC_METRICS\x10\x04\x12\x1f\n\x1b\x43P_EDGE_TOPIC_FILE_TRANSFER\x10\x05\x12\x16\n\x12\x43P_EDGE_TOPIC_INFO\x10\x06\x42/Z-gimletlabs.ai/gimlet/src/api/corepb/v1;corepbb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'src.api.corepb.v1.cp_edge_pb2', globals())
@@ -34,6 +34,8 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _EDGEHEARTBEATACK.fields_by_name['seq_id']._serialized_options = b'\342\336\037\005SeqID'
   _APPLYEXECUTIONGRAPH.fields_by_name['physical_pipeline_id']._options = None
   _APPLYEXECUTIONGRAPH.fields_by_name['physical_pipeline_id']._serialized_options = b'\342\336\037\022PhysicalPipelineID'
+  _APPLYEXECUTIONGRAPH.fields_by_name['logical_pipeline_id']._options = None
+  _APPLYEXECUTIONGRAPH.fields_by_name['logical_pipeline_id']._serialized_options = b'\342\336\037\021LogicalPipelineID'
   _DELETEEXECUTIONGRAPH.fields_by_name['physical_pipeline_id']._options = None
   _DELETEEXECUTIONGRAPH.fields_by_name['physical_pipeline_id']._serialized_options = b'\342\336\037\022PhysicalPipelineID'
   _EXECUTIONGRAPHSTATUSUPDATE.fields_by_name['physical_pipeline_id']._options = None
@@ -48,12 +50,12 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _EDGECPMETADATA.fields_by_name['device_id']._serialized_options = b'\342\336\037\010DeviceID'
   _CPEDGEMETADATA.fields_by_name['device_id']._options = None
   _CPEDGEMETADATA.fields_by_name['device_id']._serialized_options = b'\342\336\037\010DeviceID'
-  _EXECUTIONGRAPHSTATE._serialized_start=3201
-  _EXECUTIONGRAPHSTATE._serialized_end=3519
-  _EDGECPTOPIC._serialized_start=3522
-  _EDGECPTOPIC._serialized_end=3721
-  _CPEDGETOPIC._serialized_start=3724
-  _CPEDGETOPIC._serialized_end=3923
+  _EXECUTIONGRAPHSTATE._serialized_start=3289
+  _EXECUTIONGRAPHSTATE._serialized_end=3607
+  _EDGECPTOPIC._serialized_start=3610
+  _EDGECPTOPIC._serialized_end=3809
+  _CPEDGETOPIC._serialized_start=3812
+  _CPEDGETOPIC._serialized_end=4011
   _EDGEHEARTBEAT._serialized_start=289
   _EDGEHEARTBEAT._serialized_end=338
   _EDGEHEARTBEATACK._serialized_start=340
@@ -67,41 +69,41 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _EXECUTIONGRAPHSTATUS._serialized_start=607
   _EXECUTIONGRAPHSTATUS._serialized_end=748
   _APPLYEXECUTIONGRAPH._serialized_start=751
-  _APPLYEXECUTIONGRAPH._serialized_end=929
-  _DELETEEXECUTIONGRAPH._serialized_start=931
-  _DELETEEXECUTIONGRAPH._serialized_end=1044
-  _EXECUTIONGRAPHSTATUSUPDATE._serialized_start=1047
-  _EXECUTIONGRAPHSTATUSUPDATE._serialized_end=1238
-  _VIDEOSTREAMSTART._serialized_start=1240
-  _VIDEOSTREAMSTART._serialized_end=1258
-  _VIDEOSTREAMSTOP._serialized_start=1260
-  _VIDEOSTREAMSTOP._serialized_end=1277
-  _VIDEOSTREAMKEEPALIVE._serialized_start=1279
-  _VIDEOSTREAMKEEPALIVE._serialized_end=1301
-  _EDGEOTELMETRICS._serialized_start=1303
-  _EDGEOTELMETRICS._serialized_end=1412
-  _FILETRANSFERREQUEST._serialized_start=1415
-  _FILETRANSFERREQUEST._serialized_end=1563
-  _FILETRANSFERRESPONSE._serialized_start=1566
-  _FILETRANSFERRESPONSE._serialized_end=1837
-  _FILETRANSFERRESPONSE_FILECHUNK._serialized_start=1767
-  _FILETRANSFERRESPONSE_FILECHUNK._serialized_end=1837
-  _DEVICECAPABILITIES._serialized_start=1840
-  _DEVICECAPABILITIES._serialized_end=2528
-  _DEVICECAPABILITIES_MODELRUNTIMEINFO._serialized_start=2048
-  _DEVICECAPABILITIES_MODELRUNTIMEINFO._serialized_end=2284
-  _DEVICECAPABILITIES_MODELRUNTIMEINFO_MODELRUNTIMETYPE._serialized_start=2168
-  _DEVICECAPABILITIES_MODELRUNTIMEINFO_MODELRUNTIMETYPE._serialized_end=2284
-  _DEVICECAPABILITIES_CAMERAINFO._serialized_start=2287
-  _DEVICECAPABILITIES_CAMERAINFO._serialized_end=2528
-  _DEVICECAPABILITIES_CAMERAINFO_CAMERADRIVER._serialized_start=2438
-  _DEVICECAPABILITIES_CAMERAINFO_CAMERADRIVER._serialized_end=2528
-  _EDGECPMETADATA._serialized_start=2531
-  _EDGECPMETADATA._serialized_end=2735
-  _EDGECPMESSAGE._serialized_start=2737
-  _EDGECPMESSAGE._serialized_end=2863
-  _CPEDGEMETADATA._serialized_start=2866
-  _CPEDGEMETADATA._serialized_end=3070
-  _CPEDGEMESSAGE._serialized_start=3072
-  _CPEDGEMESSAGE._serialized_end=3198
+  _APPLYEXECUTIONGRAPH._serialized_end=1017
+  _DELETEEXECUTIONGRAPH._serialized_start=1019
+  _DELETEEXECUTIONGRAPH._serialized_end=1132
+  _EXECUTIONGRAPHSTATUSUPDATE._serialized_start=1135
+  _EXECUTIONGRAPHSTATUSUPDATE._serialized_end=1326
+  _VIDEOSTREAMSTART._serialized_start=1328
+  _VIDEOSTREAMSTART._serialized_end=1346
+  _VIDEOSTREAMSTOP._serialized_start=1348
+  _VIDEOSTREAMSTOP._serialized_end=1365
+  _VIDEOSTREAMKEEPALIVE._serialized_start=1367
+  _VIDEOSTREAMKEEPALIVE._serialized_end=1389
+  _EDGEOTELMETRICS._serialized_start=1391
+  _EDGEOTELMETRICS._serialized_end=1500
+  _FILETRANSFERREQUEST._serialized_start=1503
+  _FILETRANSFERREQUEST._serialized_end=1651
+  _FILETRANSFERRESPONSE._serialized_start=1654
+  _FILETRANSFERRESPONSE._serialized_end=1925
+  _FILETRANSFERRESPONSE_FILECHUNK._serialized_start=1855
+  _FILETRANSFERRESPONSE_FILECHUNK._serialized_end=1925
+  _DEVICECAPABILITIES._serialized_start=1928
+  _DEVICECAPABILITIES._serialized_end=2616
+  _DEVICECAPABILITIES_MODELRUNTIMEINFO._serialized_start=2136
+  _DEVICECAPABILITIES_MODELRUNTIMEINFO._serialized_end=2372
+  _DEVICECAPABILITIES_MODELRUNTIMEINFO_MODELRUNTIMETYPE._serialized_start=2256
+  _DEVICECAPABILITIES_MODELRUNTIMEINFO_MODELRUNTIMETYPE._serialized_end=2372
+  _DEVICECAPABILITIES_CAMERAINFO._serialized_start=2375
+  _DEVICECAPABILITIES_CAMERAINFO._serialized_end=2616
+  _DEVICECAPABILITIES_CAMERAINFO_CAMERADRIVER._serialized_start=2526
+  _DEVICECAPABILITIES_CAMERAINFO_CAMERADRIVER._serialized_end=2616
+  _EDGECPMETADATA._serialized_start=2619
+  _EDGECPMETADATA._serialized_end=2823
+  _EDGECPMESSAGE._serialized_start=2825
+  _EDGECPMESSAGE._serialized_end=2951
+  _CPEDGEMETADATA._serialized_start=2954
+  _CPEDGEMETADATA._serialized_end=3158
+  _CPEDGEMESSAGE._serialized_start=3160
+  _CPEDGEMESSAGE._serialized_end=3286
 # @@protoc_insertion_point(module_scope)
