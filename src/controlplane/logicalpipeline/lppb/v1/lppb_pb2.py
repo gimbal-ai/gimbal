94b7d355fa39c721af7cdd95c5372761393efd9e
Vihang Mehta <vihang@gimletlabs.ai>
2024-06-06 13:30:28 -0700

    Add gRPC support for new logical pipelines (#3534)
    Summary: Adds new columns to store the yaml and parsed proto.
    
    Relevant Issues: Towards GML-916
    
    Type of change: /kind feature
    
    Test Plan: Existing tests pass, will add more.
    
    Signed-off-by: Vihang Mehta <vihang@gimletlabs.ai>
    GitOrigin-RevId: c5c0036ac948d32633a52c568ea3bede0d8d01d7

--- src/controlplane/logicalpipeline/lppb/v1/lppb_pb2.py
+++ src/controlplane/logicalpipeline/lppb/v1/lppb_pb2.py
@@ -16,7 +16,7 @@ from src.common.typespb import uuid_pb2 as src_dot_common_dot_typespb_dot_uuid__
 from src.api.corepb.v1 import model_exec_pb2 as src_dot_api_dot_corepb_dot_v1_dot_model__exec__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n3src/controlplane/logicalpipeline/lppb/v1/lppb.proto\x12,gml.internal.controlplane.logicalpipeline.v1\x1a\x14gogoproto/gogo.proto\x1a\x1dsrc/common/typespb/uuid.proto\x1a\"src/api/corepb/v1/model_exec.proto\"R\n\x13LogicalPipelineInfo\x12\'\n\x02id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x06\xe2\xde\x1f\x02IDR\x02id\x12\x12\n\x04name\x18\x02 \x01(\tR\x04name\"P\n\x1bListLogicalPipelinesRequest\x12\x31\n\x06org_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\t\xe2\xde\x1f\x05OrgIDR\x05orgId\"\x7f\n\x1cListLogicalPipelinesResponse\x12_\n\tpipelines\x18\x01 \x03(\x0b\x32\x41.gml.internal.controlplane.logicalpipeline.v1.LogicalPipelineInfoR\tpipelines\"D\n\x19GetLogicalPipelineRequest\x12\'\n\x02id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x06\xe2\xde\x1f\x02IDR\x02id\"\xc4\x01\n\x1aGetLogicalPipelineResponse\x12>\n\x08pipeline\x18\x01 \x01(\x0b\x32\".gml.internal.api.core.v1.PipelineR\x08pipeline\x12\x66\n\rpipeline_info\x18\x02 \x01(\x0b\x32\x41.gml.internal.controlplane.logicalpipeline.v1.LogicalPipelineInfoR\x0cpipelineInfo\"\xa5\x01\n\x1c\x43reateLogicalPipelineRequest\x12\x31\n\x06org_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\t\xe2\xde\x1f\x05OrgIDR\x05orgId\x12\x12\n\x04name\x18\x02 \x01(\tR\x04name\x12>\n\x08pipeline\x18\x03 \x01(\x0b\x32\".gml.internal.api.core.v1.PipelineR\x08pipeline\"H\n\x1d\x43reateLogicalPipelineResponse\x12\'\n\x02id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x06\xe2\xde\x1f\x02IDR\x02id2\xa5\x04\n\x16LogicalPipelineService\x12\xad\x01\n\x14ListLogicalPipelines\x12I.gml.internal.controlplane.logicalpipeline.v1.ListLogicalPipelinesRequest\x1aJ.gml.internal.controlplane.logicalpipeline.v1.ListLogicalPipelinesResponse\x12\xa7\x01\n\x12GetLogicalPipeline\x12G.gml.internal.controlplane.logicalpipeline.v1.GetLogicalPipelineRequest\x1aH.gml.internal.controlplane.logicalpipeline.v1.GetLogicalPipelineResponse\x12\xb0\x01\n\x15\x43reateLogicalPipeline\x12J.gml.internal.controlplane.logicalpipeline.v1.CreateLogicalPipelineRequest\x1aK.gml.internal.controlplane.logicalpipeline.v1.CreateLogicalPipelineResponseBDZBgimletlabs.ai/gimlet/src/controlplane/logicalpipeline/lppb/v1;lppbb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n3src/controlplane/logicalpipeline/lppb/v1/lppb.proto\x12,gml.internal.controlplane.logicalpipeline.v1\x1a\x14gogoproto/gogo.proto\x1a\x1dsrc/common/typespb/uuid.proto\x1a\"src/api/corepb/v1/model_exec.proto\"R\n\x13LogicalPipelineInfo\x12\'\n\x02id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x06\xe2\xde\x1f\x02IDR\x02id\x12\x12\n\x04name\x18\x02 \x01(\tR\x04name\"P\n\x1bListLogicalPipelinesRequest\x12\x31\n\x06org_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\t\xe2\xde\x1f\x05OrgIDR\x05orgId\"\x7f\n\x1cListLogicalPipelinesResponse\x12_\n\tpipelines\x18\x01 \x03(\x0b\x32\x41.gml.internal.controlplane.logicalpipeline.v1.LogicalPipelineInfoR\tpipelines\"D\n\x19GetLogicalPipelineRequest\x12\'\n\x02id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x06\xe2\xde\x1f\x02IDR\x02id\"\xb2\x02\n\x1aGetLogicalPipelineResponse\x12\x42\n\x08pipeline\x18\x01 \x01(\x0b\x32\".gml.internal.api.core.v1.PipelineB\x02\x18\x01R\x08pipeline\x12\x66\n\rpipeline_info\x18\x02 \x01(\x0b\x32\x41.gml.internal.controlplane.logicalpipeline.v1.LogicalPipelineInfoR\x0cpipelineInfo\x12\x12\n\x04yaml\x18\x03 \x01(\tR\x04yaml\x12T\n\x10logical_pipeline\x18\x04 \x01(\x0b\x32).gml.internal.api.core.v1.LogicalPipelineR\x0flogicalPipeline\"\xbd\x01\n\x1c\x43reateLogicalPipelineRequest\x12\x31\n\x06org_id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\t\xe2\xde\x1f\x05OrgIDR\x05orgId\x12\x12\n\x04name\x18\x02 \x01(\tR\x04name\x12\x42\n\x08pipeline\x18\x03 \x01(\x0b\x32\".gml.internal.api.core.v1.PipelineB\x02\x18\x01R\x08pipeline\x12\x12\n\x04yaml\x18\x04 \x01(\tR\x04yaml\"H\n\x1d\x43reateLogicalPipelineResponse\x12\'\n\x02id\x18\x01 \x01(\x0b\x32\x0f.gml.types.UUIDB\x06\xe2\xde\x1f\x02IDR\x02id2\xa5\x04\n\x16LogicalPipelineService\x12\xad\x01\n\x14ListLogicalPipelines\x12I.gml.internal.controlplane.logicalpipeline.v1.ListLogicalPipelinesRequest\x1aJ.gml.internal.controlplane.logicalpipeline.v1.ListLogicalPipelinesResponse\x12\xa7\x01\n\x12GetLogicalPipeline\x12G.gml.internal.controlplane.logicalpipeline.v1.GetLogicalPipelineRequest\x1aH.gml.internal.controlplane.logicalpipeline.v1.GetLogicalPipelineResponse\x12\xb0\x01\n\x15\x43reateLogicalPipeline\x12J.gml.internal.controlplane.logicalpipeline.v1.CreateLogicalPipelineRequest\x1aK.gml.internal.controlplane.logicalpipeline.v1.CreateLogicalPipelineResponseBDZBgimletlabs.ai/gimlet/src/controlplane/logicalpipeline/lppb/v1;lppbb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'src.controlplane.logicalpipeline.lppb.v1.lppb_pb2', globals())
@@ -30,8 +30,12 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _LISTLOGICALPIPELINESREQUEST.fields_by_name['org_id']._serialized_options = b'\342\336\037\005OrgID'
   _GETLOGICALPIPELINEREQUEST.fields_by_name['id']._options = None
   _GETLOGICALPIPELINEREQUEST.fields_by_name['id']._serialized_options = b'\342\336\037\002ID'
+  _GETLOGICALPIPELINERESPONSE.fields_by_name['pipeline']._options = None
+  _GETLOGICALPIPELINERESPONSE.fields_by_name['pipeline']._serialized_options = b'\030\001'
   _CREATELOGICALPIPELINEREQUEST.fields_by_name['org_id']._options = None
   _CREATELOGICALPIPELINEREQUEST.fields_by_name['org_id']._serialized_options = b'\342\336\037\005OrgID'
+  _CREATELOGICALPIPELINEREQUEST.fields_by_name['pipeline']._options = None
+  _CREATELOGICALPIPELINEREQUEST.fields_by_name['pipeline']._serialized_options = b'\030\001'
   _CREATELOGICALPIPELINERESPONSE.fields_by_name['id']._options = None
   _CREATELOGICALPIPELINERESPONSE.fields_by_name['id']._serialized_options = b'\342\336\037\002ID'
   _LOGICALPIPELINEINFO._serialized_start=190
@@ -43,11 +47,11 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _GETLOGICALPIPELINEREQUEST._serialized_start=485
   _GETLOGICALPIPELINEREQUEST._serialized_end=553
   _GETLOGICALPIPELINERESPONSE._serialized_start=556
-  _GETLOGICALPIPELINERESPONSE._serialized_end=752
-  _CREATELOGICALPIPELINEREQUEST._serialized_start=755
-  _CREATELOGICALPIPELINEREQUEST._serialized_end=920
-  _CREATELOGICALPIPELINERESPONSE._serialized_start=922
-  _CREATELOGICALPIPELINERESPONSE._serialized_end=994
-  _LOGICALPIPELINESERVICE._serialized_start=997
-  _LOGICALPIPELINESERVICE._serialized_end=1546
+  _GETLOGICALPIPELINERESPONSE._serialized_end=862
+  _CREATELOGICALPIPELINEREQUEST._serialized_start=865
+  _CREATELOGICALPIPELINEREQUEST._serialized_end=1054
+  _CREATELOGICALPIPELINERESPONSE._serialized_start=1056
+  _CREATELOGICALPIPELINERESPONSE._serialized_end=1128
+  _LOGICALPIPELINESERVICE._serialized_start=1131
+  _LOGICALPIPELINESERVICE._serialized_end=1680
 # @@protoc_insertion_point(module_scope)
--- src/controlplane/logicalpipeline/lppb/v1/lppb_pb2.pyi
+++ src/controlplane/logicalpipeline/lppb/v1/lppb_pb2.pyi
@@ -9,14 +9,16 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class CreateLogicalPipelineRequest(_message.Message):
-    __slots__ = ["name", "org_id", "pipeline"]
+    __slots__ = ["name", "org_id", "pipeline", "yaml"]
     NAME_FIELD_NUMBER: _ClassVar[int]
     ORG_ID_FIELD_NUMBER: _ClassVar[int]
     PIPELINE_FIELD_NUMBER: _ClassVar[int]
+    YAML_FIELD_NUMBER: _ClassVar[int]
     name: str
     org_id: _uuid_pb2.UUID
     pipeline: _model_exec_pb2.Pipeline
-    def __init__(self, org_id: _Optional[_Union[_uuid_pb2.UUID, _Mapping]] = ..., name: _Optional[str] = ..., pipeline: _Optional[_Union[_model_exec_pb2.Pipeline, _Mapping]] = ...) -> None: ...
+    yaml: str
+    def __init__(self, org_id: _Optional[_Union[_uuid_pb2.UUID, _Mapping]] = ..., name: _Optional[str] = ..., pipeline: _Optional[_Union[_model_exec_pb2.Pipeline, _Mapping]] = ..., yaml: _Optional[str] = ...) -> None: ...
 
 class CreateLogicalPipelineResponse(_message.Message):
     __slots__ = ["id"]
@@ -31,12 +33,16 @@ class GetLogicalPipelineRequest(_message.Message):
     def __init__(self, id: _Optional[_Union[_uuid_pb2.UUID, _Mapping]] = ...) -> None: ...
 
 class GetLogicalPipelineResponse(_message.Message):
-    __slots__ = ["pipeline", "pipeline_info"]
+    __slots__ = ["logical_pipeline", "pipeline", "pipeline_info", "yaml"]
+    LOGICAL_PIPELINE_FIELD_NUMBER: _ClassVar[int]
     PIPELINE_FIELD_NUMBER: _ClassVar[int]
     PIPELINE_INFO_FIELD_NUMBER: _ClassVar[int]
+    YAML_FIELD_NUMBER: _ClassVar[int]
+    logical_pipeline: _model_exec_pb2.LogicalPipeline
     pipeline: _model_exec_pb2.Pipeline
     pipeline_info: LogicalPipelineInfo
-    def __init__(self, pipeline: _Optional[_Union[_model_exec_pb2.Pipeline, _Mapping]] = ..., pipeline_info: _Optional[_Union[LogicalPipelineInfo, _Mapping]] = ...) -> None: ...
+    yaml: str
+    def __init__(self, pipeline: _Optional[_Union[_model_exec_pb2.Pipeline, _Mapping]] = ..., pipeline_info: _Optional[_Union[LogicalPipelineInfo, _Mapping]] = ..., yaml: _Optional[str] = ..., logical_pipeline: _Optional[_Union[_model_exec_pb2.LogicalPipeline, _Mapping]] = ...) -> None: ...
 
 class ListLogicalPipelinesRequest(_message.Message):
     __slots__ = ["org_id"]
