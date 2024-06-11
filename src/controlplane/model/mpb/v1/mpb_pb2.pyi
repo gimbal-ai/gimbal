551847efa3f4effa09407208a5054f6f2cb48a44
Vihang Mehta <vihang@gimletlabs.ai>
2024-06-10 20:34:14 -0700

    feature: Add stub for a model service (#3599)
    Summary: TSIA
    
    Relevant Issues: Towards GML-952
    
    Type of change: /kind feature
    
    Test Plan: This is just a stub, it builds correctly. Will add unit tests
    once we implement it.
    
    Signed-off-by: Vihang Mehta <vihang@gimletlabs.ai>
    GitOrigin-RevId: 0396ba58b2d480ed89a521d297e1efded5d3296c

---
+++ src/controlplane/model/mpb/v1/mpb_pb2.pyi
@@ -0,0 +1,38 @@
+from gogoproto import gogo_pb2 as _gogo_pb2
+from src.common.typespb import uuid_pb2 as _uuid_pb2
+from src.api.corepb.v1 import model_exec_pb2 as _model_exec_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class CreateModelRequest(_message.Message):
+    __slots__ = ["model_info", "name", "org_id"]
+    MODEL_INFO_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    ORG_ID_FIELD_NUMBER: _ClassVar[int]
+    model_info: _model_exec_pb2.ModelInfo
+    name: str
+    org_id: _uuid_pb2.UUID
+    def __init__(self, org_id: _Optional[_Union[_uuid_pb2.UUID, _Mapping]] = ..., name: _Optional[str] = ..., model_info: _Optional[_Union[_model_exec_pb2.ModelInfo, _Mapping]] = ...) -> None: ...
+
+class CreateModelResponse(_message.Message):
+    __slots__ = ["id"]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    id: _uuid_pb2.UUID
+    def __init__(self, id: _Optional[_Union[_uuid_pb2.UUID, _Mapping]] = ...) -> None: ...
+
+class GetModelRequest(_message.Message):
+    __slots__ = ["id", "name"]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    id: _uuid_pb2.UUID
+    name: str
+    def __init__(self, id: _Optional[_Union[_uuid_pb2.UUID, _Mapping]] = ..., name: _Optional[str] = ...) -> None: ...
+
+class GetModelResponse(_message.Message):
+    __slots__ = ["model_info"]
+    MODEL_INFO_FIELD_NUMBER: _ClassVar[int]
+    model_info: _model_exec_pb2.ModelInfo
+    def __init__(self, model_info: _Optional[_Union[_model_exec_pb2.ModelInfo, _Mapping]] = ...) -> None: ...
