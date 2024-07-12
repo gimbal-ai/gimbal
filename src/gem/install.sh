gimbal-ai/gimbal

commit a020a4e5830d486a639c4a15d51585b04ec06d26

Add RTSP stream env var to install script (#4061)
Summary: This adds the rtsp stream env var to the install script.

Relevant Issues: towards GML-1110

Type of change: /kind feature

Test Plan: run install script, check that the GEM logs that it is using
the rtsp stream as its camera

Signed-off-by: Michelle Nguyen <michelle@gimletlabs.ai>
GitOrigin-RevId: e2bdbe08ba7e4daa608a59cef8bcea5fe5011188

src/gem/install.sh
@@ -97,6 +97,7 @@ common_docker_flags=(
   # Mount /sys so that GEM can use the mac address as the SERIAL_NUMBER and also read system metrics.
   -v /sys:/host/sys
 )
+
 extra_docker_flags=()
 DEV_MODE=${GML_DEV_MODE:-"false"}
 
@@ -129,13 +130,18 @@ cmdline_opts=(
   "--sys_class_net_path" "/host/sys/class/net"
 )
 
-VIDEO_FILE=${GML_VIDEO_FILE:-""}
+VIDEO_FILE="${GML_VIDEO_FILE:-""}"
 if [[ -n "$VIDEO_FILE" ]]; then
   video_filename=$(basename "$VIDEO_FILE")
   extra_docker_flags+=(
     -v "$VIDEO_FILE:/gml/${video_filename}"
   )
-  cmdline_opts+=("--video_file=/gml/${video_filename}")
+  cmdline_opts+=("--video_source=/gml/${video_filename}")
+fi
+
+RTSP_STREAM="${GML_RTSP_STREAM:-""}"
+if [ -n "$RTSP_STREAM" ] && [ -z "$VIDEO_FILE" ]; then
+  cmdline_opts+=("--video_source=${RTSP_STREAM}")
 fi
 
 RANDOMIZE_DEVICE_SERIAL=${GML_RANDOMIZE_DEVICE_SERIAL:-"false"}
@@ -144,7 +150,7 @@ if [[ "$RANDOMIZE_DEVICE_SERIAL" == "true" ]]; then
 fi
 
 function add_device_flags() {
-  if [[ -n "$VIDEO_FILE" ]]; then
+  if [ -n "$VIDEO_FILE" ] || [ -n "$GML_RTSP_STREAM" ]; then
     return
   fi
   ret=0

src/gem/capabilities/plugin/opencv_cam/capability_lister.cc
@@ -27,16 +27,17 @@
 
 namespace gml::gem::capabilities::opencv_cam {
 
-DEFINE_string(video_file, gflags::StringFromEnv("GML_VIDEO_FILE", ""),
-              "A video file to use as the input instead of a camera attached to the system.");
+DEFINE_string(video_source, gflags::StringFromEnv("GML_VIDEO_SOURCE", ""),
+              "A video source to use as the input instead of a camera attached to the system. For "
+              "example a video file or a RSTP stream.");
 
 Status CapabilityLister::Populate(DeviceCapabilities* cap) {
-  if (FLAGS_video_file != "") {
+  if (FLAGS_video_source != "") {
     auto mutable_cam = cap->add_cameras();
     mutable_cam->set_driver(
         internal::api::core::v1::DeviceCapabilities::CameraInfo::CAMERA_DRIVER_V4L2);
 
-    mutable_cam->set_camera_id(FLAGS_video_file);
+    mutable_cam->set_camera_id(FLAGS_video_source);
     return Status::OK();
   }
 

commit ad87e845aa016a48890f6a805a49f5f489b01c55

[gem] install.sh: bug-fix on RTSP_STREAM variable (#4111)
Summary: This fixes a bug in the install script that was introduced in
#4061, where the script tries to access GML_RTSP_STREAM even if it is
not set. Fix is simply to use RTSP_STREAM instead.

Relevant Issues: N/A

Type of change: /kind bugfix

Test Plan: Ran the script.

Signed-off-by: Omid Azizi <oazizi@gimletlabs.ai>
GitOrigin-RevId: b594cb38e2d785021f669c6753fa555cf035b491

src/gem/install.sh
@@ -149,7 +149,7 @@ if [[ "$RANDOMIZE_DEVICE_SERIAL" == "true" ]]; then
 fi
 
 function add_device_flags() {
-  if [ -n "$VIDEO_FILE" ] || [ -n "$GML_RTSP_STREAM" ]; then
+  if [ -n "$VIDEO_FILE" ] || [ -n "$RTSP_STREAM" ]; then
     return
   fi
   ret=0