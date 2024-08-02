524c1824901df575510c6f4a1992a53b2c05d6ae
Michelle Nguyen <michelle@gimletlabs.ai>
2024-08-01 19:10:28 -0700

    Add TextStream sink calculator (#4332)
    Summary: This adds a calculator which serves as a textstream sink for
    sending text to the mediastream callback.
    
    Relevant Issues: N/A
    
    Type of change: /kind feature
    
    Test Plan: add unit test
    
    Signed-off-by: Michelle Nguyen <michelle@gimletlabs.ai>
    GitOrigin-RevId: 83f4ac5735687268e7a87790a576205541a5dc95

---
+++ src/gem/calculators/core/text_stream_sink_calculator.cc
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023- Gimlet Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "src/gem/calculators/core/text_stream_sink_calculator.h"
+
+#include <memory>
+
+#include <mediapipe/framework/calculator_framework.h>
+#include <mediapipe/framework/calculator_registry.h>
+
+#include "src/api/corepb/v1/mediastream.pb.h"
+#include "src/common/base/base.h"
+
+namespace gml::gem::calculators::core {
+
+using ::gml::internal::api::core::v1::TextBatch;
+
+constexpr std::string_view kTextTag = "TEXT";
+constexpr std::string_view kEOSTag = "FINISHED";
+
+absl::Status TextStreamSinkCalculator::GetContract(mediapipe::CalculatorContract* cc) {
+  GML_ABSL_RETURN_IF_ERROR(core::ControlExecutionContextCalculator::UpdateContract(cc));
+
+  cc->Inputs().Tag(kTextTag).Set<std::string>();
+  cc->Inputs().Tag(kEOSTag).Set<bool>();
+
+  return absl::OkStatus();
+}
+
+Status TextStreamSinkCalculator::ProcessImpl(mediapipe::CalculatorContext* cc,
+                                             exec::core::ControlExecutionContext* control_ctx) {
+  std::vector<std::unique_ptr<google::protobuf::Message>> messages;
+
+  auto batch = std::make_unique<TextBatch>();
+  if (cc->Inputs().HasTag(kTextTag)) {
+    batch->set_text(cc->Inputs().Tag(kTextTag).Get<std::string>());
+  }
+  if (cc->Inputs().HasTag(kEOSTag)) {
+    batch->set_eos(cc->Inputs().Tag(kEOSTag).Get<bool>());
+  }
+  messages.push_back(std::move(batch));
+
+  auto cb = control_ctx->GetMediaStreamCallback();
+  if (!!cb) {
+    GML_RETURN_IF_ERROR(cb(messages));
+  }
+  return Status::OK();
+}
+
+REGISTER_CALCULATOR(TextStreamSinkCalculator);
+
+}  // namespace gml::gem::calculators::core
---
+++ src/gem/calculators/core/text_stream_sink_calculator_test.cc
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2023- Gimlet Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "src/gem/calculators/core/text_stream_sink_calculator.h"
+
+#include "gmock/gmock.h"
+#include <absl/container/flat_hash_map.h>
+#include <mediapipe/framework/calculator_runner.h>
+#include <mediapipe/framework/packet.h>
+
+#include "src/api/corepb/v1/mediastream.pb.h"
+#include "src/common/testing/protobuf.h"
+#include "src/common/testing/testing.h"
+#include "src/gem/calculators/core/test_utils.h"
+#include "src/gem/testing/core/calculator_tester.h"
+
+namespace gml::gem::calculators::core {
+
+using ::gml::internal::api::core::v1::TextBatch;
+
+struct TextStreamSinkTestCase {
+  std::string text;
+  bool eos;
+};
+
+class TextStreamSinkCalculatorTest : public ::testing::TestWithParam<TextStreamSinkTestCase> {};
+
+TEST_P(TextStreamSinkCalculatorTest, SendsCallback) {
+  auto packet_and_expectation = GetParam();
+
+  constexpr char kTextStreamSinkNode[] = R"pbtxt(
+    calculator: "TextStreamSinkCalculator"
+    input_side_packet: "EXEC_CTX:ctrl_exec_ctx"
+    input_stream: "TEXT:text"
+    input_stream: "FINISHED:eos"
+  )pbtxt";
+
+  mediapipe::CalculatorRunner runner(kTextStreamSinkNode);
+
+  bool callbackCalled = false;
+  exec::core::ControlExecutionContext::MediaStreamCallback cb =
+      [&](const std::vector<std::unique_ptr<google::protobuf::Message>>& messages) {
+        callbackCalled = true;
+        EXPECT_EQ(messages.size(), 1);
+        auto type = messages[0]->GetTypeName();
+        EXPECT_EQ(type, TextBatch::descriptor()->full_name());
+        auto text_batch = static_cast<const TextBatch&>(*messages[0]);
+        EXPECT_EQ(text_batch.text(), packet_and_expectation.text);
+        EXPECT_EQ(text_batch.eos(), packet_and_expectation.eos);
+
+        return Status::OK();
+      };
+
+  exec::core::ControlExecutionContext control_ctx;
+  control_ctx.RegisterMediaStreamCallback(cb);
+
+  runner.MutableSidePackets()->Get("EXEC_CTX", 0) =
+      mediapipe::MakePacket<exec::core::ExecutionContext*>(&control_ctx);
+
+  mediapipe::Packet p = mediapipe::MakePacket<std::string>(packet_and_expectation.text);
+  p = p.At(mediapipe::Timestamp(static_cast<int64_t>(0)));
+  runner.MutableInputs()->Tag("TEXT").packets.push_back(p);
+
+  mediapipe::Packet p2 = mediapipe::MakePacket<bool>(packet_and_expectation.eos);
+  p2 = p2.At(mediapipe::Timestamp(static_cast<int64_t>(0)));
+  runner.MutableInputs()->Tag("FINISHED").packets.push_back(p2);
+
+  ASSERT_OK(runner.Run());
+
+  EXPECT_TRUE(callbackCalled);
+}
+
+INSTANTIATE_TEST_SUITE_P(TextStreamSinkCalculatorTestSuite, TextStreamSinkCalculatorTest,
+                         ::testing::Values(TextStreamSinkTestCase{
+                             "this is my text",
+                             true,
+                         }));
+
+}  // namespace gml::gem::calculators::core
