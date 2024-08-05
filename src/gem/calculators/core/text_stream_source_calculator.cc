b66e369c7604387c602c3639a36ffed4f366341b
Michelle Nguyen <michelle@gimletlabs.ai>
2024-08-04 22:08:09 -0700

    Add calculator for getting input from textStream (#4324)
    Summary: This adds a calculator which reads from the mediastream control
    queue and sends that as its output.
    
    Relevant Issues: N/A
    
    Type of change: /kind feature
    
    Test Plan: attempted to add a unit test, however it seems the mediapipe
    runner runs processImpl in an infinite loop if there are no expected
    inputs. I debated adding an input just for testing, which worked, but
    seemed hacky. The other test (opencv_cam_calculator_test) for a
    calculator without inputs takes a max_num_frames which ends the
    execution.
    
    Signed-off-by: Michelle Nguyen <michelle@gimletlabs.ai>
    GitOrigin-RevId: 7a94b26cf62cf821abc765828d3803b01812d304

---
+++ src/gem/calculators/core/text_stream_source_calculator.cc
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023- Gimlet Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "src/gem/calculators/core/text_stream_source_calculator.h"
+
+#include <memory>
+
+#include <mediapipe/framework/calculator_framework.h>
+#include <mediapipe/framework/calculator_registry.h>
+
+#include "src/api/corepb/v1/mediastream.pb.h"
+#include "src/common/base/base.h"
+
+namespace gml::gem::calculators::core {
+
+constexpr std::string_view kPromptTag = "PROMPT";
+
+absl::Status TextStreamSourceCalculator::GetContract(mediapipe::CalculatorContract* cc) {
+  GML_ABSL_RETURN_IF_ERROR(core::ControlExecutionContextCalculator::UpdateContract(cc));
+
+  cc->Outputs().Tag(kPromptTag).Set<std::string>();
+
+  return absl::OkStatus();
+}
+
+Status TextStreamSourceCalculator::ProcessImpl(mediapipe::CalculatorContext* cc,
+                                               exec::core::ControlExecutionContext* control_ctx) {
+  auto ctrl_message = control_ctx->GetControlMessage();
+  if (!ctrl_message) {
+    return Status::OK();
+  }
+  auto prompt = ctrl_message->text_stream_control().prompt();
+
+  // Use now as the timestamp.
+  auto now = std::chrono::steady_clock::now();
+  auto time_since_epoch = now.time_since_epoch();
+  auto start_time_unix_ms =
+      std::chrono::duration_cast<std::chrono::microseconds>(time_since_epoch).count();
+
+  cc->Outputs()
+      .Tag(kPromptTag)
+      .Add(new std::string(prompt), mediapipe::Timestamp(start_time_unix_ms));
+
+  return Status::OK();
+}
+
+REGISTER_CALCULATOR(TextStreamSourceCalculator);
+
+}  // namespace gml::gem::calculators::core
