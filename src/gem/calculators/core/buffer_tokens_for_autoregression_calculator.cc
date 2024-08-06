76d4e5fce90ce278140b0d08fc742767546d5100
James Bartlett <james@gimletlabs.ai>
2024-08-05 23:39:42 -0700

    [gem] Add BufferTokensForAutoregression calculator (#4425)
    Summary: Adds a calculator to buffer inputs for autoregressive language
    models. Also adds the MLIR op definition for the calculator.
    
    Relevant Issues: Towards GML-1283.
    
    Type of change: /kind feature
    
    Test Plan: Added a test for the calculator.
    
    Signed-off-by: James Bartlett <james@gimletlabs.ai>
    GitOrigin-RevId: c0c5ac1a32c8d59f25a0ff11409f521aed5c6010

---
+++ src/gem/calculators/core/buffer_tokens_for_autoregression_calculator.cc
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023- Gimlet Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "src/gem/calculators/core/buffer_tokens_for_autoregression_calculator.h"
+
+#include <memory>
+
+#include <mediapipe/framework/calculator_registry.h>
+
+#include "src/common/base/base.h"
+
+namespace gml::gem::calculators::core {
+using ::gml::internal::api::core::v1::Detection;
+
+constexpr std::string_view kTokensTag = "TOKENS";
+constexpr std::string_view kLoopStartTag = "LOOP_START";
+constexpr std::string_view kAllTokensTag = "ALL_TOKENS";
+
+absl::Status BufferTokensForAutoregressionCalculator::GetContract(
+    mediapipe::CalculatorContract* cc) {
+  cc->Inputs().Tag(kTokensTag).Set<std::vector<int>>();
+  cc->Inputs().Tag(kLoopStartTag).Set<bool>();
+  cc->Outputs().Tag(kAllTokensTag).Set<std::vector<int>>();
+  cc->SetTimestampOffset(0);
+  return absl::OkStatus();
+}
+
+absl::Status BufferTokensForAutoregressionCalculator::Open(mediapipe::CalculatorContext*) {
+  return absl::OkStatus();
+}
+
+absl::Status BufferTokensForAutoregressionCalculator::Process(mediapipe::CalculatorContext* cc) {
+  const auto& tokens = cc->Inputs().Tag(kTokensTag).Get<std::vector<int>>();
+  auto loop_start = cc->Inputs().Tag(kLoopStartTag).Get<bool>();
+
+  if (loop_start) {
+    all_tokens_.clear();
+  }
+  for (auto tok : tokens) {
+    all_tokens_.push_back(tok);
+  }
+
+  cc->Outputs()
+      .Tag(kAllTokensTag)
+      .AddPacket(mediapipe::MakePacket<std::vector<int>>(all_tokens_).At(cc->InputTimestamp()));
+  return absl::OkStatus();
+}
+
+absl::Status BufferTokensForAutoregressionCalculator::Close(mediapipe::CalculatorContext*) {
+  return absl::OkStatus();
+}
+
+REGISTER_CALCULATOR(BufferTokensForAutoregressionCalculator);
+
+}  // namespace gml::gem::calculators::core
---
+++ src/gem/calculators/core/buffer_tokens_for_autoregression_calculator_test.cc
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2023- Gimlet Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "src/gem/calculators/core/buffer_tokens_for_autoregression_calculator.h"
+
+#include <google/protobuf/text_format.h>
+
+#include "src/common/testing/testing.h"
+#include "src/gem/testing/core/calculator_tester.h"
+
+namespace gml::gem::calculators::core {
+
+constexpr std::string_view kBufferTokensForAutoregressionNode = R"pbtxt(
+calculator: "BufferTokensForAutoregressionCalculator"
+input_stream: "TOKENS:tokens"
+input_stream: "LOOP_START:loop_start"
+output_stream: "ALL_TOKENS:all_tokens"
+)pbtxt";
+
+struct BufferTokensForAutoregressionTestCase {
+  std::vector<std::vector<int>> token_inputs;
+  std::vector<bool> loop_starts;
+
+  std::vector<std::vector<int>> expected_all_tokens;
+};
+
+class BufferTokensForAutoregressionTest
+    : public ::testing::TestWithParam<BufferTokensForAutoregressionTestCase> {};
+
+TEST_P(BufferTokensForAutoregressionTest, ConvertsCorrectly) {
+  auto test_case = GetParam();
+
+  std::string config(kBufferTokensForAutoregressionNode);
+  testing::CalculatorTester tester(config);
+
+  for (size_t i = 0; i < test_case.token_inputs.size(); ++i) {
+    auto ts = mediapipe::Timestamp(i);
+    tester.ForInput("TOKENS", test_case.token_inputs[i], ts);
+    tester.ForInput<bool>("LOOP_START", test_case.loop_starts[i], ts);
+  }
+  tester.Run();
+  for (size_t i = 0; i < test_case.expected_all_tokens.size(); ++i) {
+    auto ts = mediapipe::Timestamp(i);
+    tester.ExpectOutput<std::vector<int>>("ALL_TOKENS", 0, ts, test_case.expected_all_tokens[i]);
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    BufferTokensForAutoregressionTestSuite, BufferTokensForAutoregressionTest,
+    ::testing::Values(
+        BufferTokensForAutoregressionTestCase{
+            .token_inputs = {{1, 2, 3, 4, 5}, {6}, {7}},
+            .loop_starts =
+                {
+                    true,
+                    false,
+                    false,
+                },
+            .expected_all_tokens =
+                {
+                    {1, 2, 3, 4, 5},
+                    {1, 2, 3, 4, 5, 6},
+                    {1, 2, 3, 4, 5, 6, 7},
+                },
+        },
+        BufferTokensForAutoregressionTestCase{
+            .token_inputs = {{1, 2, 3, 4, 5}, {6}, {7}, {10, 11, 12, 13}, {1}, {1}},
+            .loop_starts =
+                {
+                    true,
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                },
+            .expected_all_tokens =
+                {
+                    {1, 2, 3, 4, 5},
+                    {1, 2, 3, 4, 5, 6},
+                    {1, 2, 3, 4, 5, 6, 7},
+                    {10, 11, 12, 13},
+                    {10, 11, 12, 13, 1},
+                    {10, 11, 12, 13, 1, 1},
+                },
+        }));
+
+}  // namespace gml::gem::calculators::core
